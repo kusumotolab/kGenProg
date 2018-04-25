0fc288cdae72bf85ae7835fbd181d2694defed68
mtj0928 <mtj0928@gmail.com>
2018-04-25 23:59:47 +0900

    TreeGene の実装を追加 #11

--- src/main/java/jp/kusumotolab/kgenprog/ga/Gene.java
+++ src/main/java/jp/kusumotolab/kgenprog/ga/Gene.java
@@ -12,17 +12,32 @@ public abstract class Gene {
 class TreeGene extends Gene {
 	private Gene parent;
 	private Base base;
-	
-	@Override
-	public List<Base> getBases() {
-		
-		
-		return parent.getBases();
+
+	public TreeGene() {
+		this(null, null);
+	}
+
+	public TreeGene(Gene parent, Base base) {
+		this.parent = parent;
+		this.base = base;
 	}
 
 	@Override
-	public List<Gene> generateNextGenerationGenes(List<Base> bases) {
-		// TODO Auto-generated method stub
-		return null;
+	public List<Base> getBases() {
+		if (parent == null) {
+			return new ArrayList<>();
+		}
+		final List<Base> bases = parent instanceof TreeGene ? parent.getBases() : new ArrayList<>(parent.getBases());
+		bases.add(base);
+		return bases;
+	}
+
+	@Override
+	public List<Gene> generateNextGenerationGenes(final List<Base> bases) {
+		final List<Gene> genes = new ArrayList<>();
+		for (Base base : bases) {
+			genes.add(new TreeGene(this, base));
+		}
+		return genes;
 	}
 }
