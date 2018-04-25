6933e4555c9d60b81b7112b4dd22695bc4247596
mtj0928 <mtj0928@gmail.com>
2018-04-25 20:41:16 +0900

    add RandomNumberGeneration. #7

---
+++ src/main/java/jp/kusumotolab/kgenprog/ga/RandomNumberGeneration.java
@@ -0,0 +1,28 @@
+package jp.kusumotolab.kgenprog.ga;
+
+import java.util.Random;
+
+public class RandomNumberGeneration {
+
+    public static RandomNumberGeneration sharedInstance = new RandomNumberGeneration();
+
+    private int seed;
+    private Random random;
+
+    private RandomNumberGeneration() {
+        setSeed(0);
+    }
+
+    public void setSeed(int seed) {
+        this.seed = seed;
+        random = new Random(seed);
+    }
+
+    public int getSeed() {
+        return seed;
+    }
+
+    public int getRandomNumber() {
+        return random.nextInt();
+    }
+}
