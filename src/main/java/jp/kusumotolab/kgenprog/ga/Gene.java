158e0f9e121122059c8f7523e3cbb5795720c879
mtj0928 <mtj0928@gmail.com>
2018-04-30 17:46:36 +0900

    不要になったimport文の削除 #11

--- src/main/java/jp/kusumotolab/kgenprog/ga/Gene.java
+++ src/main/java/jp/kusumotolab/kgenprog/ga/Gene.java
@@ -1,6 +1,5 @@
 package jp.kusumotolab.kgenprog.ga;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public abstract class Gene {
@@ -8,4 +7,3 @@ public abstract class Gene {
     public abstract List<Base> getBases();
     public abstract List<Gene> generateNextGenerationGenes(List<Base> bases);
 }
-
