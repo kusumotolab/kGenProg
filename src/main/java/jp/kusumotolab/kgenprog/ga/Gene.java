1ab07e317ed16eb2321b3a01dc70dccab69de4f6
mtj0928 <mtj0928@gmail.com>
2018-04-30 17:33:14 +0900

    TreeGeneをpublicに変更 #11

--- src/main/java/jp/kusumotolab/kgenprog/ga/Gene.java
+++ src/main/java/jp/kusumotolab/kgenprog/ga/Gene.java
@@ -9,35 +9,3 @@ public abstract class Gene {
     public abstract List<Gene> generateNextGenerationGenes(List<Base> bases);
 }
 
-class TreeGene extends Gene {
-	private Gene parent;
-	private Base base;
-
-	public TreeGene() {
-		this(null, null);
-	}
-
-	public TreeGene(Gene parent, Base base) {
-		this.parent = parent;
-		this.base = base;
-	}
-
-	@Override
-	public List<Base> getBases() {
-		if (parent == null) {
-			return new ArrayList<>();
-		}
-		final List<Base> bases = parent instanceof TreeGene ? parent.getBases() : new ArrayList<>(parent.getBases());
-		bases.add(base);
-		return bases;
-	}
-
-	@Override
-	public List<Gene> generateNextGenerationGenes(final List<Base> bases) {
-		final List<Gene> genes = new ArrayList<>();
-		for (Base base : bases) {
-			genes.add(new TreeGene(this, base));
-		}
-		return genes;
-	}
-}
---
+++ src/main/java/jp/kusumotolab/kgenprog/ga/TreeGene.java
@@ -0,0 +1,37 @@
+package jp.kusumotolab.kgenprog.ga;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TreeGene extends Gene {
+	private Gene parent;
+	private Base base;
+
+	public TreeGene() {
+		this(null, null);
+	}
+
+	public TreeGene(Gene parent, Base base) {
+		this.parent = parent;
+		this.base = base;
+	}
+
+	@Override
+	public List<Base> getBases() {
+		if (parent == null) {
+			return new ArrayList<>();
+		}
+		final List<Base> bases = parent instanceof TreeGene ? parent.getBases() : new ArrayList<>(parent.getBases());
+		bases.add(base);
+		return bases;
+	}
+
+	@Override
+	public List<Gene> generateNextGenerationGenes(final List<Base> bases) {
+		final List<Gene> genes = new ArrayList<>();
+		for (Base base : bases) {
+			genes.add(new TreeGene(this, base));
+		}
+		return genes;
+	}
+}
