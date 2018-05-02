2e1e5a527f105cfc4539ddec798f0db9540cbd84
shinsuke-mat <shinsuke-mat@users.noreply.github.com>
2018-05-02 14:34:56 +0900

    TestExecutor作成 #2
    必要最低限を実装．
    pending: targetProjectクラスの利用，Locationクラスの〃，TestCaseクラスの〃

--- src/main/java/jp/kusumotolab/kgenprog/project/test/TestExecutor.java
+++ src/main/java/jp/kusumotolab/kgenprog/project/test/TestExecutor.java
@@ -1,8 +1,22 @@
 package jp.kusumotolab.kgenprog.project.test;
 
+import java.io.IOException;
+import java.util.List;
+
+import org.junit.runner.JUnitCore;
+import org.junit.runner.Request;
+import org.junit.runner.Result;
+
 import jp.kusumotolab.kgenprog.project.GeneratedSourceCode;
 import jp.kusumotolab.kgenprog.project.TargetProject;
 
+/**
+ * テスト実行クラス．
+ * 外部プロジェクトの単体テストclassファイルを実行してその結果を回収する．
+ * 
+ * @author shinsuke
+ *
+ */
 public class TestExecutor {
 
 	private TargetProject targetProject;
@@ -15,4 +29,36 @@ public class TestExecutor {
 		return null;
 
 	}
+
+	/**
+	 * exec(GeneratedSourceCode)の代替メソッド．
+	 * 本メソッドがGeneratedSourceCodeの実装に強依存なので，ひとまずStringで処理．
+	 * 
+	 * @param classPath
+	 *            対象プロジェクトのclassPath
+	 * @param testClasses
+	 *            実行対象のテストクラス名の集合
+	 * @return
+	 */
+	public TestResults exec(String classPath, List<String> testClasses) {
+		TestResults testResults = new TestResults();
+		JUnitCore junit = new JUnitCore();
+		try {
+			ClassPathHacker.addFile(classPath);
+			for(String testClass: testClasses) {
+				Result result = junit.run(Request.classes(Class.forName(testClass)));
+				testResults.add(testClass, result);
+			}
+			return testResults;
+		} catch (IOException e) {
+			// TODO 自動生成された catch ブロック
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			// TODO 自動生成された catch ブロック
+			e.printStackTrace();
+		}
+
+		return null;
+	}
+
 }
--- src/main/java/jp/kusumotolab/kgenprog/project/test/TestResult.java
+++ src/main/java/jp/kusumotolab/kgenprog/project/test/TestResult.java
@@ -2,18 +2,30 @@ package jp.kusumotolab.kgenprog.project.test;
 
 import java.util.Map;
 
+import org.junit.runner.Result;
+
 import jp.kusumotolab.kgenprog.project.Location;
 
 public class TestResult {
-	private boolean isSuccess;
-	private Map<Location,Integer> executionCount;
+	private Map<Location, Integer> executionCount;
 	private TestCase testCase;
-	private String message;
+	private Result junitRunnerResult;
 
-	public TestResult(boolean isSuccess, Map<Location, Integer> executionCount, TestCase testCase, String message) {
-		this.isSuccess = isSuccess;
-		this.executionCount = executionCount;
+	public TestResult(TestCase testCase, Result junitRunnerResult, Map<Location, Integer> executionCount) {
 		this.testCase = testCase;
-		this.message = message;
+		this.junitRunnerResult = junitRunnerResult;
+		this.executionCount = executionCount;
+	}
+
+	public boolean wasSuccessful() {
+		return junitRunnerResult.wasSuccessful();
+	}
+
+	public int getFailureCount() {
+		return junitRunnerResult.getFailureCount();
+	}
+
+	public int getRunCount() {
+		return junitRunnerResult.getRunCount();
 	}
 }
