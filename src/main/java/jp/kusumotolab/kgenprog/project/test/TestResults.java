bc2434644ca0eedf9485fe9f89cf58b68e3a7c2b
shinsuke-mat <shinsuke-mat@users.noreply.github.com>
2018-05-02 14:34:56 +0900

    TestExecutor作成 #2
    必要最低限を実装．
    pending: targetProjectクラスの利用，Locationクラスの〃，TestCaseクラスの〃

--- src/main/java/jp/kusumotolab/kgenprog/project/test/TestResults.java
+++ src/main/java/jp/kusumotolab/kgenprog/project/test/TestResults.java
@@ -1,46 +1,83 @@
 package jp.kusumotolab.kgenprog.project.test;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+
+import org.junit.runner.Result;
 
 import jp.kusumotolab.kgenprog.project.Location;
 
 public class TestResults {
-	private List<TestResult> testResults;
 
-	//	hitori
-	public List<TestResult> getFailedTestResults(){
-		return null;
+	private final List<TestResult> testResults;
+
+	public TestResults() {
+		testResults = new ArrayList<>();
 	}
 
-	//	hitori
-	public List<TestResult> getSuccessedTestResults(){
-		return null;
+	/**
+	 * 失敗したテスト結果の一覧を取得．
+	 * 
+	 * @return 失敗したテスト結果s
+	 */
+	public List<TestResult> getFailedTestResults() {
+		final List<TestResult> testResults = new ArrayList<>();
+		for (final TestResult testResult : this.testResults) {
+			if (testResult.wasSuccessful()) {
+				continue;
+			}
+			testResults.add(testResult);
+		}
+		return testResults;
 	}
 
-	//	hitori
-	public double getSuccessRate(){
-		return Double.NaN;
+	// necessary?
+	public List<TestResult> getSuccessedTestResults() {
+		final List<TestResult> testResults = new ArrayList<>();
+		for (final TestResult testResult : this.testResults) {
+			if (! testResult.wasSuccessful()) {
+				continue;
+			}
+			testResults.add(testResult);
+		}
+		return testResults;
 	}
 
-	//hitori
+	/**
+	 * テストの成功率
+	 * @return
+	 */
+	public double getSuccessRate() {
+		final int fail = getFailedTestResults().size();
+		final int success = getSuccessedTestResults().size();
+		return success / (success + fail);
+	}
+
 	public Map<Location, Integer> getExecutedFailedTestCounts() {
 		return null;
 	}
 
-	//hitori
 	public Map<Location, Integer> getNotExecutedFailedTestCounts() {
 		return null;
 	}
 
-	//hitori
 	public Map<Location, Integer> getExecutedPassedTestCounts() {
 		return null;
 	}
 
-	//hitori
 	public Map<Location, Integer> getNotExecutedPassedTestCounts() {
 		return null;
+	}
+
+	public void add(final String testClass, final Result result) {
+		final TestCase testCase = getTestCase(testClass); // stub
+		testResults.add(new TestResult(testCase, result, null));
+	}
+
+	// stub for generating TestCase
+	private TestCase getTestCase(final String testClass) {
+		return new TestCase(null, testClass, -1);
 	}
 
 }
