479b3b10560532a1b0d81097feb1ca083117a212
Ryo Arima <a3636tako@gmail.com>
2018-05-06 23:55:10 +0900

    ASTのノードを削除するOperation実装

---
+++ src/test/java/jp/kusumotolab/kgenprog/project/jdt/DeleteOperationTest.java
@@ -0,0 +1,38 @@
+package jp.kusumotolab.kgenprog.project.jdt;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.Collections;
+
+import org.eclipse.jdt.core.dom.MethodDeclaration;
+import org.eclipse.jdt.core.dom.Statement;
+import org.eclipse.jdt.core.dom.TypeDeclaration;
+import org.junit.Test;
+
+import jp.kusumotolab.kgenprog.project.GeneratedSourceCode;
+import jp.kusumotolab.kgenprog.project.SourceFile;
+
+public class DeleteOperationTest {
+
+	@Test
+	public void testDeleteStatement() {
+		String testSource = "class A{public void a(){int a = 0;a = 1;}}";
+		SourceFile testSourceFile = new SourceFile("A.java");
+		
+		JDTASTConstruction constructor = new JDTASTConstruction();
+		GeneratedJDTAST ast = (GeneratedJDTAST)constructor.constructAST(testSourceFile, testSource.toCharArray());
+		GeneratedSourceCode generatedSourceCode = new GeneratedSourceCode(Collections.singletonList(ast));
+		
+		TypeDeclaration type = (TypeDeclaration)ast.getRoot().types().get(0);
+		MethodDeclaration method = type.getMethods()[0];
+		Statement statement = (Statement)method.getBody().statements().get(1);
+		JDTLocation location = new JDTLocation(testSourceFile, statement);
+		DeleteOperation operation = new DeleteOperation();
+		
+		GeneratedSourceCode code = operation.apply(generatedSourceCode, location);
+		GeneratedJDTAST newAST = (GeneratedJDTAST)code.getFiles().get(0);
+		assertEquals("class A {\n  public void a(){\n    int a=0;\n  }\n}\n", newAST.getRoot().toString());
+	
+	}
+
+}
