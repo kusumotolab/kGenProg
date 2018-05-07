9efec436f3f4b9f4f577642abbe4a61b1ef15e72
YoshikiHigo <yoshiki.higo@gmail.com>
2018-05-07 16:32:23 +0900

    対象プロジェクトのビルド機能を追加．現時点では差分ビルドはできていない． #1

--- src/main/java/jp/kusumotolab/kgenprog/project/GeneratedSourceCode.java
+++ src/main/java/jp/kusumotolab/kgenprog/project/GeneratedSourceCode.java
@@ -1,16 +1,20 @@
 package jp.kusumotolab.kgenprog.project;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
- * APR によって生成されたソースコード
- * 複数ソースファイルの AST の集合を持つ
+ * APR によって生成されたソースコード 複数ソースファイルの AST の集合を持つ
  */
 public class GeneratedSourceCode {
 	private List<GeneratedAST> files;
 
-	public GeneratedSourceCode(List<GeneratedAST> files){
+	public GeneratedSourceCode(List<GeneratedAST> files) {
 		this.files = files;
 	}
 
+	public List<GeneratedAST> getGeneratedASTs(){
+		final List<GeneratedAST> asts = new ArrayList<>(this.files);
+		return asts;
+	}
 }
--- src/main/java/jp/kusumotolab/kgenprog/project/jdt/GeneratedJDTAST.java
+++ src/main/java/jp/kusumotolab/kgenprog/project/jdt/GeneratedJDTAST.java
@@ -7,5 +7,15 @@ import jp.kusumotolab.kgenprog.project.GeneratedAST;
 public class GeneratedJDTAST implements GeneratedAST {
 	private CompilationUnit root;
 
+	@Override
+	public String getSourceCode() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
+	@Override
+	public String getPrimaryClassName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 }
