eb2b2431cc7be874b9370b188437be1697fc2594
yoshi <yoshi@192.168.146.113>
2018-05-07 16:32:23 +0900

    対象プロジェクトのビルド機能を追加．現時点では差分ビルドはできていない． #1

--- src/main/java/jp/kusumotolab/kgenprog/project/TargetProject.java
+++ src/main/java/jp/kusumotolab/kgenprog/project/TargetProject.java
@@ -1,5 +1,6 @@
 package jp.kusumotolab.kgenprog.project;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
@@ -9,17 +10,17 @@ import jp.kusumotolab.kgenprog.ga.SimpleGene;
 import jp.kusumotolab.kgenprog.ga.Variant;
 
 public class TargetProject {
-	private List<SourceFile> sourceFiles;
-	private List<SourceFile> testFiles;
-	private List<ClassPath> classPaths;
+	private final List<SourceFile> sourceFiles;
+	private final List<SourceFile> testFiles;
+	private final List<ClassPath> classPaths;
 
-	public TargetProject(List<SourceFile> sourceFiles, List<SourceFile> testFiles, List<ClassPath> classPaths){
+	public TargetProject(List<SourceFile> sourceFiles, List<SourceFile> testFiles, List<ClassPath> classPaths) {
 		this.sourceFiles = sourceFiles;
 		this.testFiles = testFiles;
 		this.classPaths = classPaths;
 	}
 
-	public Variant getInitialVariant(){
+	public Variant getInitialVariant() {
 		Gene gene = new SimpleGene(Collections.emptyList());
 		Fitness fitness = null;
 		GeneratedSourceCode generatedSourceCode = new GeneratedSourceCode(constructAST());
@@ -27,8 +28,23 @@ public class TargetProject {
 		return new Variant(gene, fitness, generatedSourceCode);
 	}
 
-//	hitori
-	private List<GeneratedAST> constructAST(){
+	// hitori
+	private List<GeneratedAST> constructAST() {
 		return null;
+	}
+
+	public List<SourceFile> getSourceFiles() {
+		final List<SourceFile> sourceFiles = new ArrayList<>(this.sourceFiles);
+		return sourceFiles;
+	}
+
+	public List<SourceFile> getTestFiles() {
+		final List<SourceFile> testFiles = new ArrayList<>(this.testFiles);
+		return testFiles;
+	}
+	
+	public List<ClassPath> getClassPaths(){
+		final List<ClassPath> classpaths = new ArrayList<>(this.classPaths);
+		return classpaths;
 	}
 }
