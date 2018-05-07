3ebef576813bdc2483d045d17aabbc3080b10c5c
yoshi <yoshi@192.168.146.113>
2018-05-07 16:32:23 +0900

    対象プロジェクトのビルド機能を追加．現時点では差分ビルドはできていない． #1

--- src/main/java/jp/kusumotolab/kgenprog/KGenProgMain.java
+++ src/main/java/jp/kusumotolab/kgenprog/KGenProgMain.java
@@ -27,10 +27,11 @@ public class KGenProgMain {
 	private SourceCodeGeneration sourceCodeGeneration;
 	private SourceCodeValidation sourceCodeValidation;
 	private VariantSelection variantSelection;
+	private ProjectBuilder projectBuilder;
 	private TestExecutor testExecutor;
 
-	public KGenProgMain(TargetProject targetProject, FaultLocalization faultLocalization, Mutation mutation, Crossover crossover,
-			SourceCodeGeneration sourceCodeGeneration, SourceCodeValidation sourceCodeValidation,
+	public KGenProgMain(TargetProject targetProject, FaultLocalization faultLocalization, Mutation mutation,
+			Crossover crossover, SourceCodeGeneration sourceCodeGeneration, SourceCodeValidation sourceCodeValidation,
 			VariantSelection variantSelection) {
 		this.targetProject = targetProject;
 		this.faultLocalization = faultLocalization;
@@ -39,8 +40,9 @@ public class KGenProgMain {
 		this.sourceCodeGeneration = sourceCodeGeneration;
 		this.sourceCodeValidation = sourceCodeValidation;
 		this.variantSelection = variantSelection;
-
+		this.projectBuilder = new ProjectBuilder(targetProject);
 		this.testExecutor = new TestExecutor(targetProject);
+
 	}
 
 	public void run() {
@@ -75,19 +77,19 @@ public class KGenProgMain {
 		}
 	}
 
-	//	hitori
+	// hitori
 	private boolean reachedMaxGeneration() {
 		// TODO Auto-generated method stub
 		return false;
 	}
 
-	//	hitori
+	// hitori
 	private boolean isTimedOut() {
 		// TODO Auto-generated method stub
 		return false;
 	}
 
-	//	hitori
+	// hitori
 	private boolean isSuccess(List<Variant> variants) {
 		return false;
 	}
