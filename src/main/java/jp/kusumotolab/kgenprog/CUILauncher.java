264a8f8fc2b00ec776c12cf3f42bf7ed53bb9daf
a-tanikd <8575113+a-tanikd@users.noreply.github.com>
2018-05-09 19:45:45 +0900

    [update] rewrite CUILauncher in java #13
    Revert "[update] CUILauncher を kotlinize"
    This reverts commit 8c8d8feea70cdb5ef1549da48c97d8dd400f0b8f.

---
+++ src/main/java/jp/kusumotolab/kgenprog/CUILauncher.java
@@ -0,0 +1,73 @@
+package jp.kusumotolab.kgenprog;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import jp.kusumotolab.kgenprog.fl.FaultLocalization;
+import jp.kusumotolab.kgenprog.fl.Ochiai;
+import jp.kusumotolab.kgenprog.ga.Crossover;
+import jp.kusumotolab.kgenprog.ga.DefaultCodeValidation;
+import jp.kusumotolab.kgenprog.ga.DefaultSourceCodeGeneration;
+import jp.kusumotolab.kgenprog.ga.DefaultVariantSelection;
+import jp.kusumotolab.kgenprog.ga.Mutation;
+import jp.kusumotolab.kgenprog.ga.RandomMutation;
+import jp.kusumotolab.kgenprog.ga.SiglePointCrossover;
+import jp.kusumotolab.kgenprog.ga.SourceCodeGeneration;
+import jp.kusumotolab.kgenprog.ga.SourceCodeValidation;
+import jp.kusumotolab.kgenprog.ga.VariantSelection;
+import jp.kusumotolab.kgenprog.project.ClassPath;
+import jp.kusumotolab.kgenprog.project.SourceFile;
+import jp.kusumotolab.kgenprog.project.TargetProject;
+import org.kohsuke.args4j.CmdLineException;
+import org.kohsuke.args4j.CmdLineParser;
+import org.kohsuke.args4j.Option;
+
+public class CUILauncher {
+
+	@Option(name = "-s", aliases = "--src", required = true, usage = "Paths of the root directories holding source codes")
+	public List<String> sourceFiles = Arrays.asList("src/main/java");
+	@Option(name = "-t", aliases = "--test", required = true, usage = "Paths of the root directories holding test codes")
+	public List<String> testFiles = Arrays.asList();
+	@Option(name = "-c", aliases = "--cp", required = true, usage = "Class paths required to build the target project")
+	public List<String> classPaths = Arrays.asList();
+
+	public static void main(String[] args) {
+		CUILauncher launcher = new CUILauncher();
+		CmdLineParser parser = new CmdLineParser(launcher);
+
+		try {
+			parser.parseArgument(args);
+		} catch (CmdLineException e) {
+			System.err.println(e.getMessage());
+			parser.printUsage(System.err);
+			System.exit(1);
+		}
+		launcher.launch();
+	}
+
+	public void launch() {
+		List<SourceFile> sourceFiles = this.sourceFiles.stream()
+				.map(SourceFile::new)
+				.collect(Collectors.toList());
+
+		List<SourceFile> testFiles = this.testFiles.stream()
+				.map(SourceFile::new)
+				.collect(Collectors.toList());
+
+		List<ClassPath> classPaths = this.classPaths.stream()
+				.map(ClassPath::new)
+				.collect(Collectors.toList());
+
+		TargetProject targetProject = new TargetProject(sourceFiles, testFiles, classPaths);
+		FaultLocalization faultLocalization = new Ochiai();
+		Mutation mutation = new RandomMutation();
+		Crossover crossover = new SiglePointCrossover();
+		SourceCodeGeneration sourceCodeGeneration = new DefaultSourceCodeGeneration();
+		SourceCodeValidation sourceCodeValidation = new DefaultCodeValidation();
+		VariantSelection variantSelection = new DefaultVariantSelection();
+
+		KGenProgMain kGenProgMain = new KGenProgMain(targetProject, faultLocalization, mutation, crossover, sourceCodeGeneration, sourceCodeValidation, variantSelection);
+		kGenProgMain.run();
+	}
+}
