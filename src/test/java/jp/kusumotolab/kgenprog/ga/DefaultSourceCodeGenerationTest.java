84517e3cb86d318928c8f50dce83e377b14f1be5
mtj0928 <mtj0928@gmail.com>
2018-05-09 21:01:03 +0900

    SourceCodeGenerationに処理を追加 #8

---
+++ src/test/java/jp/kusumotolab/kgenprog/ga/DefaultSourceCodeGenerationTest.java
@@ -0,0 +1,38 @@
+package jp.kusumotolab.kgenprog.ga;
+
+import jp.kusumotolab.kgenprog.project.*;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class DefaultSourceCodeGenerationTest {
+
+
+    @Test
+    public void execTest() {
+        final List<SourceFile> sourceCodeFiles = new ArrayList<>();
+        sourceCodeFiles.add(new SourceFile("example/example01/src/BuggyCalculator.java"));
+
+        final List<SourceFile> testFiles = new ArrayList<>();
+        testFiles.add(new SourceFile("example/example01/src/BuggyCalculatorTest.java"));
+
+        final List<ClassPath> classPaths = new ArrayList<>();
+
+        final TargetProject targetProject = new TargetProject(sourceCodeFiles, testFiles, classPaths);
+
+        final DefaultSourceCodeGeneration defaultSourceCodeGeneration = new DefaultSourceCodeGeneration();
+        final Gene gene = new SimpleGene(new ArrayList<>());
+
+        // TODO: None以外のOperationでテストする必要有り
+        final Base base = new Base(null, new NoneOperation());
+
+        final List<Gene> genes = gene.generateNextGenerationGenes(Collections.singletonList(base));
+
+        final GeneratedSourceCode generatedSourceCode = defaultSourceCodeGeneration.exec(genes.get(0), targetProject);
+
+        // TODO: Noneしかないのでテストができない
+        System.out.println(generatedSourceCode.getFiles().get(0).getSourceCode());
+    }
+}
