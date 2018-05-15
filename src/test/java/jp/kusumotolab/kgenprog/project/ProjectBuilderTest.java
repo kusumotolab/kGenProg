6c65bdf207b1b30e734f566d0fa30112ce3fcf61
shinsuke-mat <shinsuke-mat@users.noreply.github.com>
2018-05-15 22:04:18 +0900

    beautify

--- src/test/java/jp/kusumotolab/kgenprog/project/ProjectBuilderTest.java
+++ src/test/java/jp/kusumotolab/kgenprog/project/ProjectBuilderTest.java
@@ -3,8 +3,6 @@ package jp.kusumotolab.kgenprog.project;
 import static org.junit.Assert.assertTrue;
 
 import java.io.File;
-import java.util.ArrayList;
-import java.util.List;
 
 import org.junit.Test;
 
@@ -29,7 +27,8 @@ public class ProjectBuilderTest {
 	@Test
 	public void testBuildStringForExample02() {
 		final String separator = File.separator;
-		final TargetProject targetProject = TargetProject.generate("example/example02");		final ProjectBuilder projectBuilder = new ProjectBuilder(targetProject);
+		final TargetProject targetProject = TargetProject.generate("example/example02");
+		final ProjectBuilder projectBuilder = new ProjectBuilder(targetProject);
 		final String outDirPath = "example" + separator + "example02" + separator + "bin";
 
 		final File outputDirectoryFile = new File(outDirPath);
