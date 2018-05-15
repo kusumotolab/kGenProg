caf2e21e3208565c07f080b49e914044d75d86ea
shinsuke-mat <shinsuke-mat@users.noreply.github.com>
2018-05-15 20:38:11 +0900

    TestProcessBuilderの作成途中
    TargetProjectとの整合性がやっかい．
    SourceFileが示す相対パスからFQNを取得する必要があるが，どこからパッケージ名が開始されるかが判別できない．
    TargetProjectにBaseDirを指定させるか？

--- src/test/java/jp/kusumotolab/kgenprog/project/test/TestProcessBuilderTest.java
+++ src/test/java/jp/kusumotolab/kgenprog/project/test/TestProcessBuilderTest.java
@@ -9,6 +9,8 @@ import java.util.Arrays;
 import org.junit.Before;
 import org.junit.Test;
 
+import jp.kusumotolab.kgenprog.project.ClassPath;
+import jp.kusumotolab.kgenprog.project.SourceFile;
 import jp.kusumotolab.kgenprog.project.TargetProject;
 
 public class TestProcessBuilderTest {
@@ -22,14 +24,30 @@ public class TestProcessBuilderTest {
 
 	@Test
 	public void exec01() {
-		final TestProcessBuilder builder = new TestProcessBuilder(null);
+		final TestProcessBuilder builder = new TestProcessBuilder(createTargetProjectFromExample01());
+		builder.start();
+		/*
 		builder.start( //
 				Arrays.asList("jp.kusumotolab.BuggyCalculator"), //
 				Arrays.asList("jp.kusumotolab.BuggyCalculatorTest"), //
 				"example/example01/bin/");
-		
+		*/
 		TestResults tr = TestResults.deserialize();
 		assertEquals(4, tr.getTestResults().size());
 		assertEquals(1, tr.getFailedTestResults().size());
+	}
+	
+
+	private TargetProject createTargetProjectFromExample01() {
+		String project = "example/example01/";
+		return new TargetProject( //
+				Arrays.asList( //
+						new SourceFile(project + "src/jp/kusumotolab/BuggyCalculator.java"), //
+						new SourceFile(project + "src/jp/kusumotolab/BuggyCalculatorTest.java")), //
+				Arrays.asList( //
+						new SourceFile(project + "src/BuggyCalculatorTest.java")), //
+				Arrays.asList( //
+						new ClassPath("lib/junit4/junit-4.12.jar"), //
+						new ClassPath("lib/junit4/hamcrest-core-1.3.jar")));
 	}
 }
