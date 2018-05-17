4b7c41d8b23a1cb0bfe579c714e082c1d0f25df6
shinsuke-mat <shinsuke-mat@users.noreply.github.com>
2018-05-17 20:56:08 +0900

    TestProcessBuilder修正．最低限動くように

--- src/test/java/jp/kusumotolab/kgenprog/project/test/TestProcessBuilderTest.java
+++ src/test/java/jp/kusumotolab/kgenprog/project/test/TestProcessBuilderTest.java
@@ -1,21 +1,16 @@
 package jp.kusumotolab.kgenprog.project.test;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.Arrays;
 
 import org.junit.Before;
 import org.junit.Test;
 
-import jp.kusumotolab.kgenprog.project.ClassPath;
-import jp.kusumotolab.kgenprog.project.SourceFile;
 import jp.kusumotolab.kgenprog.project.TargetProject;
 
 public class TestProcessBuilderTest {
-
-	private static TargetProject targetProject;
 
 	@Before
 	public void before() throws IOException {
@@ -24,30 +19,13 @@ public class TestProcessBuilderTest {
 
 	@Test
 	public void exec01() {
-		final TestProcessBuilder builder = new TestProcessBuilder(createTargetProjectFromExample01());
+		final TargetProject targetProject = TargetProject.generate("example/example01");
+		final TestProcessBuilder builder = new TestProcessBuilder(targetProject);
 		builder.start();
-		/*
-		builder.start( //
-				Arrays.asList("jp.kusumotolab.BuggyCalculator"), //
-				Arrays.asList("jp.kusumotolab.BuggyCalculatorTest"), //
-				"example/example01/bin/");
-		*/
+
 		TestResults tr = TestResults.deserialize();
 		assertEquals(4, tr.getTestResults().size());
 		assertEquals(1, tr.getFailedTestResults().size());
 	}
-	
 
-	private TargetProject createTargetProjectFromExample01() {
-		String project = "example/example01/";
-		return new TargetProject( //
-				Arrays.asList( //
-						new SourceFile(project + "src/jp/kusumotolab/BuggyCalculator.java"), //
-						new SourceFile(project + "src/jp/kusumotolab/BuggyCalculatorTest.java")), //
-				Arrays.asList( //
-						new SourceFile(project + "src/BuggyCalculatorTest.java")), //
-				Arrays.asList( //
-						new ClassPath("lib/junit4/junit-4.12.jar"), //
-						new ClassPath("lib/junit4/hamcrest-core-1.3.jar")));
-	}
 }
