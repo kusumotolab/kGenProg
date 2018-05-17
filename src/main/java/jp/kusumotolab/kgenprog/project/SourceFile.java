4dc99da8025a0f56939d791efb851572e6a48f99
shinsuke-mat <shinsuke-mat@users.noreply.github.com>
2018-05-17 20:48:07 +0900

    TargetProject周りを少し改善．
    ひとまずrootとsrcPathを追加．
    あとgenerate()周りを修正．

--- src/main/java/jp/kusumotolab/kgenprog/project/SourceFile.java
+++ src/main/java/jp/kusumotolab/kgenprog/project/SourceFile.java
@@ -1,9 +1,25 @@
 package jp.kusumotolab.kgenprog.project;
 
+import java.io.File;
+
 public final class SourceFile {
+	// TODO
+	// pathはjava.nio.Pathで管理すべき．
+
 	public final String path;
 
 	public SourceFile(final String path) {
-		this.path = path;
+		this.path = new File(path).toString(); // for class-platform compatibility
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		final File f = new File(((SourceFile) o).path);
+		return new File(this.path).equals(f);
+	}
+
+	@Override
+	public String toString() {
+		return this.path;
 	}
 }
