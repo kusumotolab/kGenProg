34cbef08af4b80d28865eb4d17bcfc8129cf2393
YoshikiHigo <yoshiki.higo@gmail.com>
2018-05-25 21:04:01 +0900

    SourceFileをabstractに変更． TargetSourceFileとTestSourceFileをSourceFileの子クラスとして作成．

--- src/test/java/jp/kusumotolab/kgenprog/project/TargetProjectTest.java
+++ src/test/java/jp/kusumotolab/kgenprog/project/TargetProjectTest.java
@@ -14,11 +14,11 @@ public class TargetProjectTest {
 		final TargetProject project = TargetProject.generate(basePath);
 
 		assertThat(project.getSourceFiles(), is(containsInAnyOrder( //
-				new SourceFile(basePath + "src/jp/kusumotolab/BuggyCalculator.java"),
-				new SourceFile(basePath + "src/jp/kusumotolab/BuggyCalculatorTest.java"))));
+				new TargetSourceFile(basePath + "src/jp/kusumotolab/BuggyCalculator.java"),
+				new TargetSourceFile(basePath + "src/jp/kusumotolab/BuggyCalculatorTest.java"))));
 
 		assertThat(project.getTestFiles(), is(containsInAnyOrder( //
-				new SourceFile(basePath + "src/jp/kusumotolab/BuggyCalculatorTest.java"))));
+				new TestSourceFile(basePath + "src/jp/kusumotolab/BuggyCalculatorTest.java"))));
 	}
 
 	@Test
@@ -27,14 +27,14 @@ public class TargetProjectTest {
 		final TargetProject project = TargetProject.generate(basePath);
 
 		assertThat(project.getSourceFiles(), is(containsInAnyOrder( //
-				new SourceFile(basePath + "src/jp/kusumotolab/BuggyCalculator.java"),
-				new SourceFile(basePath + "src/jp/kusumotolab/BuggyCalculatorTest.java"),
-				new SourceFile(basePath + "src/jp/kusumotolab/Util.java"),
-				new SourceFile(basePath + "src/jp/kusumotolab/UtilTest.java"))));
+				new TargetSourceFile(basePath + "src/jp/kusumotolab/BuggyCalculator.java"),
+				new TargetSourceFile(basePath + "src/jp/kusumotolab/BuggyCalculatorTest.java"),
+				new TargetSourceFile(basePath + "src/jp/kusumotolab/Util.java"),
+				new TargetSourceFile(basePath + "src/jp/kusumotolab/UtilTest.java"))));
 
 		assertThat(project.getTestFiles(), is(containsInAnyOrder( //
-				new SourceFile(basePath + "src/jp/kusumotolab/BuggyCalculatorTest.java"),
-				new SourceFile(basePath + "src/jp/kusumotolab/UtilTest.java"))));
+				new TestSourceFile(basePath + "src/jp/kusumotolab/BuggyCalculatorTest.java"),
+				new TestSourceFile(basePath + "src/jp/kusumotolab/UtilTest.java"))));
 
 	}
 }
