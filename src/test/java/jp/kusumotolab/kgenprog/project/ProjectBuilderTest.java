e7515285b51a6a94ba835616ed1759fa9c114fb2
YoshikiHigo <yoshiki.higo@gmail.com>
2018-05-30 17:00:40 +0200

    example01とexample02用のテストも，example03用のテストに倣って変更．

--- src/test/java/jp/kusumotolab/kgenprog/project/ProjectBuilderTest.java
+++ src/test/java/jp/kusumotolab/kgenprog/project/ProjectBuilderTest.java
@@ -1,7 +1,6 @@
 package jp.kusumotolab.kgenprog.project;
 
 import static org.hamcrest.Matchers.is;
-import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertThat;
 
 import java.io.File;
@@ -22,22 +21,15 @@ public class ProjectBuilderTest {
 
 		final BuildResults buildResults = projectBuilder.build(outDirPath);
 
-		assertFalse(buildResults.isBuildFailed); // TODO assertThat に置き換える
+		assertThat(buildResults.isBuildFailed, is(false));
+		assertThat(buildResults.isMappingAvailable(), is(true));
 
 		for (final SourceFile sourceFile : targetProject.getSourceFiles()) {
-			if (sourceFile.path.endsWith("BuggyCalculator.java")) {
-				final Path pathToClass = buildResults.getPathToClasses(Paths.get(sourceFile.path)).iterator().next();
-				final Path correspondingSource = buildResults.getPathToSource(pathToClass);
-				assertThat(pathToClass.endsWith("BuggyCalculator.class"), is(true));
-				assertThat(correspondingSource.endsWith("BuggyCalculator.java"), is(true));
-			}
-			// TODO テストファイルがTargetProject#sourceFilesに含まれている前提のテスト．これでよいか？
-			if (sourceFile.path.endsWith("BuggyCalculatorTest.java")) {
-				final Path pathToClass = buildResults.getPathToClasses(Paths.get(sourceFile.path)).iterator().next();
-				final Path correspondingSource = buildResults.getPathToSource(pathToClass);
-				assertThat(pathToClass.endsWith("BuggyCalculatorTest.class"), is(true));
-				assertThat(correspondingSource.endsWith("BuggyCalculatorTest.java"), is(true));
-			}
+			final Set<Path> pathToClasses = buildResults.getPathToClasses(Paths.get(sourceFile.path));
+			pathToClasses.stream().forEach(c -> {
+				final Path correspondingSourcePath = buildResults.getPathToSource(c);
+				assertThat(correspondingSourcePath.toFile(), is(new File(sourceFile.path)));
+			});
 		}
 	}
 
@@ -50,35 +42,15 @@ public class ProjectBuilderTest {
 
 		final BuildResults buildResults = projectBuilder.build(outDirPath);
 
-		assertFalse(buildResults.isBuildFailed); // TODO assertThat に置き換える
+		assertThat(buildResults.isBuildFailed, is(false));
+		assertThat(buildResults.isMappingAvailable(), is(true));
 
 		for (final SourceFile sourceFile : targetProject.getSourceFiles()) {
-			if (sourceFile.path.endsWith("BuggyCalculator.java")) {
-				final Path pathToClass = buildResults.getPathToClasses(Paths.get(sourceFile.path)).iterator().next();
-				final Path correspondingSource = buildResults.getPathToSource(pathToClass);
-				assertThat(pathToClass.endsWith("BuggyCalculator.class"), is(true));
-				assertThat(correspondingSource.endsWith("BuggyCalculator.java"), is(true));
-			}
-			// TODO テストファイルがTargetProject#sourceFilesに含まれている前提のテスト．これでよいか？
-			if (sourceFile.path.endsWith("BuggyCalculatorTest.java")) {
-				final Path pathToClass = buildResults.getPathToClasses(Paths.get(sourceFile.path)).iterator().next();
-				final Path correspondingSource = buildResults.getPathToSource(pathToClass);
-				assertThat(pathToClass.endsWith("BuggyCalculatorTest.class"), is(true));
-				assertThat(correspondingSource.endsWith("BuggyCalculatorTest.java"), is(true));
-			}
-			if (sourceFile.path.endsWith("Util.java")) {
-				final Path pathToClass = buildResults.getPathToClasses(Paths.get(sourceFile.path)).iterator().next();
-				final Path correspondingSource = buildResults.getPathToSource(pathToClass);
-				assertThat(pathToClass.endsWith("Util.class"), is(true));
-				assertThat(correspondingSource.endsWith("Util.java"), is(true));
-			}
-			// TODO テストファイルがTargetProject#sourceFilesに含まれている前提のテスト．これでよいか？
-			if (sourceFile.path.endsWith("UtilTest.java")) {
-				final Path pathToClass = buildResults.getPathToClasses(Paths.get(sourceFile.path)).iterator().next();
-				final Path correspondingSource = buildResults.getPathToSource(pathToClass);
-				assertThat(pathToClass.endsWith("UtilTest.class"), is(true));
-				assertThat(correspondingSource.endsWith("UtilTest.java"), is(true));
-			}
+			final Set<Path> pathToClasses = buildResults.getPathToClasses(Paths.get(sourceFile.path));
+			pathToClasses.stream().forEach(c -> {
+				final Path correspondingSourcePath = buildResults.getPathToSource(c);
+				assertThat(correspondingSourcePath.toFile(), is(new File(sourceFile.path)));
+			});
 		}
 	}
 
