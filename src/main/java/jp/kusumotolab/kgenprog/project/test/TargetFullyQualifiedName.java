651638545ccec4e10b2f14a2e20395e52816c8e3
shinsuke-mat <shinsuke-mat@users.noreply.github.com>
2018-06-11 15:17:42 +0900

    fqn周りのdeprecatedメソッドの削除

--- src/main/java/jp/kusumotolab/kgenprog/project/test/TargetFullyQualifiedName.java
+++ src/main/java/jp/kusumotolab/kgenprog/project/test/TargetFullyQualifiedName.java
@@ -1,8 +1,5 @@
 package jp.kusumotolab.kgenprog.project.test;
 
-import java.nio.file.Path;
-import jp.kusumotolab.kgenprog.project.SourceFile;
-
 public class TargetFullyQualifiedName extends FullyQualifiedName {
 
   private static final long serialVersionUID = 1L;
@@ -11,13 +8,4 @@ public class TargetFullyQualifiedName extends FullyQualifiedName {
     super(value);
   }
 
-  @Deprecated
-  public TargetFullyQualifiedName(final SourceFile sourceFile) {
-    super(sourceFile);
-  }
-
-  @Deprecated
-  public TargetFullyQualifiedName(final Path path) {
-    super(path);
-  }
 }
--- src/main/java/jp/kusumotolab/kgenprog/project/test/TestFullyQualifiedName.java
+++ src/main/java/jp/kusumotolab/kgenprog/project/test/TestFullyQualifiedName.java
@@ -1,8 +1,5 @@
 package jp.kusumotolab.kgenprog.project.test;
 
-import java.nio.file.Path;
-import jp.kusumotolab.kgenprog.project.SourceFile;
-
 public class TestFullyQualifiedName extends FullyQualifiedName {
 
   private static final long serialVersionUID = 1L;
@@ -11,13 +8,4 @@ public class TestFullyQualifiedName extends FullyQualifiedName {
     super(value);
   }
 
-  @Deprecated
-  public TestFullyQualifiedName(final SourceFile sourceFile) {
-    super(sourceFile);
-  }
-
-  @Deprecated
-  public TestFullyQualifiedName(final Path path) {
-    super(path);
-  }
 }
