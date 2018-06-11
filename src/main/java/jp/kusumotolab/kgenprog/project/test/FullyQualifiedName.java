60e96b079d13dfd4738a379200f8ffc5e6f95cce
shinsuke-mat <shinsuke-mat@users.noreply.github.com>
2018-06-11 15:17:42 +0900

    fqn周りのdeprecatedメソッドの削除

--- src/main/java/jp/kusumotolab/kgenprog/project/test/FullyQualifiedName.java
+++ src/main/java/jp/kusumotolab/kgenprog/project/test/FullyQualifiedName.java
@@ -1,9 +1,6 @@
 package jp.kusumotolab.kgenprog.project.test;
 
-import java.io.File;
 import java.io.Serializable;
-import java.nio.file.Path;
-import jp.kusumotolab.kgenprog.project.SourceFile;
 
 public abstract class FullyQualifiedName implements Serializable {
 
@@ -12,21 +9,8 @@ public abstract class FullyQualifiedName implements Serializable {
   final public String value;
 
   protected FullyQualifiedName(final String value) {
-    // final String v = value.substring(0, value.lastIndexOf('.'));
-    // this.value = v.replaceAll("\\" + File.separator, ".");
+    // TODO check validation
     this.value = value;
-  }
-
-  @Deprecated
-  protected FullyQualifiedName(SourceFile sourceFile) {
-    this(sourceFile.toString().substring(0, sourceFile.toString().lastIndexOf('.')).replace("/",
-        "."));
-  }
-
-  @Deprecated
-  protected FullyQualifiedName(Path path) {
-    final String v = path.toString().substring(0, path.toString().lastIndexOf('.'));
-    this.value = v.replaceAll("\\" + File.separator, ".");
   }
 
   @Override
