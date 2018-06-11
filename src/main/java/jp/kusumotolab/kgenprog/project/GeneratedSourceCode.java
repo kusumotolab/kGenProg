1ed2c584da70c5f0edf3c9f6f90ad3884e398a19
shinsuke-mat <shinsuke-mat@users.noreply.github.com>
2018-06-11 13:25:11 +0900

    潜在的バグにTODO追加

--- src/main/java/jp/kusumotolab/kgenprog/project/GeneratedSourceCode.java
+++ src/main/java/jp/kusumotolab/kgenprog/project/GeneratedSourceCode.java
@@ -1,13 +1,12 @@
 package jp.kusumotolab.kgenprog.project;
 
-import java.util.ArrayList;
 import java.util.List;
-import jp.kusumotolab.kgenprog.project.jdt.GeneratedJDTAST;
 
 /**
  * APR によって生成されたソースコード 複数ソースファイルの AST の集合を持つ
  */
 public class GeneratedSourceCode {
+  // TODO listは順序が保証されず重複を許容してしまう．Mapで名前から引ける方が外から使いやすい．
   private List<GeneratedAST> files;
 
   public GeneratedSourceCode(List<GeneratedAST> files) {
