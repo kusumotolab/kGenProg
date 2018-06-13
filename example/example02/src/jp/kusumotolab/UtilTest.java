3ba77a98a56bbdb6d49f239ac9d903ca8e3460ad
shinsuke-mat <shinsuke-mat@users.noreply.github.com>
2018-06-13 11:45:44 +0900

    修正漏れ

--- example/example02/src/jp/kusumotolab/UtilTest.java
+++ example/example02/src/jp/kusumotolab/UtilTest.java
@@ -1,12 +1,22 @@
 package jp.kusumotolab;
-public class Util {
-  public static int plus(  int n){
-    return n + 1;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import org.junit.Test;
+public class UtilTest {
+  @Test public void plusTest01(){
+    assertEquals(11,Util.plus(10));
   }
-  public static int minus(  int n){
-    return n - 1;
+  @Test public void plusTest02(){
+    assertEquals(101,Util.plus(100));
   }
-  public static void dummy(){
-    new String();
+  @Test public void minusTest01(){
+    assertEquals(9,Util.minus(10));
+  }
+  @Test public void minusTest02(){
+    assertEquals(-1,Util.minus(0));
+  }
+  @Test public void dummyTest01(){
+    Util.dummy();
+    assertTrue(true);
   }
 }
