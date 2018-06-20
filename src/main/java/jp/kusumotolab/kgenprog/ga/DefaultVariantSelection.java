43b35a69ff3956652b005ab1814c2f1bade63083
mtj0928 <mtj0928@gmail.com>
2018-06-21 07:55:32 +0900

    exec()が返すListの順番を修正 #10

--- src/main/java/jp/kusumotolab/kgenprog/ga/DefaultVariantSelection.java
+++ src/main/java/jp/kusumotolab/kgenprog/ga/DefaultVariantSelection.java
@@ -1,10 +1,7 @@
 package jp.kusumotolab.kgenprog.ga;
 
 import java.util.Comparator;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
-import java.util.Map.Entry;
 import java.util.stream.Collectors;
 
 public class DefaultVariantSelection implements VariantSelection {
@@ -17,16 +14,9 @@ public class DefaultVariantSelection implements VariantSelection {
 
   @Override
   public List<Variant> exec(List<Variant> variants) {
-    final Map<Variant, Integer> hashMap = new HashMap<>();
-    for (int i = 0; i < variants.size(); i++) {
-      hashMap.put(variants.get(i), i);
-    }
-    final List<Variant> list = hashMap.entrySet().stream()
-        .sorted((o1, o2) ->
-            o2.getKey().getFitness().getValue() - o1.getKey().getFitness().getValue() > 0 ? 1 : -1)
+    final List<Variant> list = variants.stream()
+        .sorted(Comparator.comparingDouble(e -> ((Variant) e).getFitness().getValue()).reversed())
         .limit(maxVariantsPerGeneration)
-        .sorted(Comparator.comparingInt(Map.Entry::getValue))
-        .map(Entry::getKey)
         .collect(Collectors.toList());
     return list;
   }
