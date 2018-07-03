c2a96a2886ee9247d20ab37d96277cb596891ad9
Ryo Arima <r-arima@ist.osaka-u.ac.jp>
2018-07-04 02:47:14 +0900

    pull up apply method

--- src/main/java/jp/kusumotolab/kgenprog/project/jdt/DeleteOperation.java
+++ src/main/java/jp/kusumotolab/kgenprog/project/jdt/DeleteOperation.java
@@ -1,26 +1,17 @@
 package jp.kusumotolab.kgenprog.project.jdt;
 
-import java.util.List;
-import java.util.stream.Collectors;
 import org.eclipse.jdt.core.dom.ASTNode;
 import org.eclipse.jdt.core.dom.CompilationUnit;
 import org.eclipse.jdt.core.dom.rewrite.ASTRewrite;
-import org.eclipse.jface.text.BadLocationException;
-import org.eclipse.jface.text.Document;
-import org.eclipse.text.edits.MalformedTreeException;
-import org.eclipse.text.edits.TextEdit;
-import jp.kusumotolab.kgenprog.project.GeneratedAST;
 import jp.kusumotolab.kgenprog.project.GeneratedSourceCode;
 import jp.kusumotolab.kgenprog.project.Location;
 
 public class DeleteOperation implements JDTOperation {
 
   @Override
-  public GeneratedSourceCode apply(final GeneratedSourceCode generatedSourceCode,
-      final Location location) {
-    final List<GeneratedAST> newASTs = generatedSourceCode.getFiles().stream()
-        .map(ast -> applyDeleteOperation(ast, location)).collect(Collectors.toList());
-    return new GeneratedSourceCode(newASTs);
+  public void applyToASTRewrite(final GeneratedJDTAST ast, final JDTLocation location,
+      final ASTRewrite astRewrite) {
+    astRewrite.remove(location.locate(ast.getRoot()), null);
   }
 
   @Override
@@ -40,27 +31,5 @@ public class DeleteOperation implements JDTOperation {
     return generatedSourceCode;
   }
 
-  private GeneratedAST applyDeleteOperation(final GeneratedAST ast, final Location location) {
-    if (!ast.getSourceFile().equals(location.getSourceFile())) {
-      return ast;
-    }
 
-    final JDTLocation jdtLocation = (JDTLocation) location;
-    final GeneratedJDTAST jdtast = (GeneratedJDTAST) ast;
-    final CompilationUnit root = jdtast.getRoot();
-    final ASTNode deleteLocation = jdtLocation.locate(root);
-
-    final ASTRewrite astRewrite = ASTRewrite.create(root.getAST());
-    astRewrite.remove(deleteLocation, null);
-
-    final Document document = new Document(jdtast.getSourceCode());
-    final TextEdit edit = astRewrite.rewriteAST(document, null);
-    try {
-      edit.apply(document);
-    } catch (MalformedTreeException | BadLocationException e) {
-      throw new RuntimeException(e);
-    }
-
-    return jdtast.getConstruction().constructAST(ast.getSourceFile(), document.get());
-  }
 }
