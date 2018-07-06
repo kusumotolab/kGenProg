6f625dea96acfc9f485320f24510a6103631a739
shinsuke-mat <shinsuke-mat@users.noreply.github.com>
2018-07-06 17:51:52 +0900

    ビルド失敗時のバグを修正 - TestProcessBuilderでBuildResults#isBuildFailedを参照してOptional.empty()を返すように - DefaultCodeValidationでのビルド失敗時のテストを追加

--- src/test/java/jp/kusumotolab/kgenprog/ga/DefaultCodeValidationTest.java
+++ src/test/java/jp/kusumotolab/kgenprog/ga/DefaultCodeValidationTest.java
@@ -5,11 +5,11 @@ import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import org.junit.Test;
 import jp.kusumotolab.kgenprog.project.GeneratedSourceCode;
 import jp.kusumotolab.kgenprog.project.factory.TargetProject;
 import jp.kusumotolab.kgenprog.project.factory.TargetProjectFactory;
 import jp.kusumotolab.kgenprog.project.test.TestProcessBuilder;
-import org.junit.Test;
 
 public class DefaultCodeValidationTest {
 
@@ -27,5 +27,22 @@ public class DefaultCodeValidationTest {
     final Fitness fitness =
         defaultCodeValidation.exec(generatedSourceCode, targetProject, testProcessBuilder);
     assertThat(fitness.getValue(), is(closeTo(0.75, 0.000001)));
+  }
+
+  @Test
+  public void testExecForBuildFailure() {
+    final Path rootDir = Paths.get("example/example00");
+    final Path outDir = rootDir.resolve("_bin");
+
+    final TargetProject targetProject = TargetProjectFactory.create(rootDir);
+    final TestProcessBuilder testProcessBuilder = new TestProcessBuilder(targetProject, outDir);
+    final Variant initialVariant = targetProject.getInitialVariant();
+    final GeneratedSourceCode generatedSourceCode = initialVariant.getGeneratedSourceCode();
+
+    final DefaultCodeValidation defaultCodeValidation = new DefaultCodeValidation();
+    final Fitness fitness =
+        defaultCodeValidation.exec(generatedSourceCode, targetProject, testProcessBuilder);
+
+    assertThat(fitness.getValue(), is(Double.NaN));
   }
 }
