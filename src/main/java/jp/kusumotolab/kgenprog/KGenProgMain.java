52948389fe77f78aa6654eab46fd4f1001a51485
Yoshiki Higo <yoshiki.higo@gmail.com>
2018-07-08 13:08:28 +0900

    必要数のバグ修正プログラムが生成した場合，制限時間に達成した場合，最大世代数に到達した場合にGAが終了するようにした

--- src/main/java/jp/kusumotolab/kgenprog/KGenProgMain.java
+++ src/main/java/jp/kusumotolab/kgenprog/KGenProgMain.java
@@ -4,6 +4,7 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 import jp.kusumotolab.kgenprog.fl.FaultLocalization;
 import jp.kusumotolab.kgenprog.fl.Suspiciouseness;
 import jp.kusumotolab.kgenprog.ga.Base;
@@ -29,6 +30,12 @@ public class KGenProgMain {
   private SourceCodeValidation sourceCodeValidation;
   private VariantSelection variantSelection;
   private TestProcessBuilder testProcessBuilder;
+  private final List<Variant> completedVariants;
+
+  // 以下，一時的なフィールド #146 で解決すべき
+  private final long timeout;
+  private final int maxGeneration;
+  private final int requiredSolutions;
 
   // TODO #146
   // workingdirのパスを一時的にMainに記述
@@ -38,6 +45,15 @@ public class KGenProgMain {
   public KGenProgMain(TargetProject targetProject, FaultLocalization faultLocalization,
       Mutation mutation, Crossover crossover, SourceCodeGeneration sourceCodeGeneration,
       SourceCodeValidation sourceCodeValidation, VariantSelection variantSelection) {
+    
+    this(targetProject, faultLocalization, mutation, crossover, sourceCodeGeneration,
+        sourceCodeValidation, variantSelection, 600, 10, 1);
+  }
+
+  public KGenProgMain(TargetProject targetProject, FaultLocalization faultLocalization,
+      Mutation mutation, Crossover crossover, SourceCodeGeneration sourceCodeGeneration,
+      SourceCodeValidation sourceCodeValidation, VariantSelection variantSelection,
+      final long timeout, final int maxGeneration, final int requiredSolutions) {
     this.targetProject = targetProject;
     this.faultLocalization = faultLocalization;
     this.mutation = mutation;
@@ -46,6 +62,11 @@ public class KGenProgMain {
     this.sourceCodeValidation = sourceCodeValidation;
     this.variantSelection = variantSelection;
     this.testProcessBuilder = new TestProcessBuilder(targetProject, WORKING_DIR);
+    this.completedVariants = new ArrayList<>();
+
+    this.timeout = timeout;
+    this.maxGeneration = maxGeneration;
+    this.requiredSolutions = requiredSolutions;
   }
 
   public void run() {
@@ -53,10 +74,16 @@ public class KGenProgMain {
     final Variant initialVariant = targetProject.getInitialVariant();
     selectedVariants.add(initialVariant);
     mutation.setCandidates(initialVariant.getGeneratedSourceCode().getFiles());
+
+    final long startTime = System.nanoTime();
+    int generation = 0;
     while (true) {
-      if (isTimedOut() || reachedMaxGeneration() || isSuccess(selectedVariants)) {
+
+      // 制限時間に達したか，最大世代数に到達した場合には GA を抜ける
+      if (isTimedOut(startTime) || reachedMaxGeneration(generation++)) {
         break;
       }
+
       List<Gene> genes = new ArrayList<>();
       for (Variant variant : selectedVariants) {
         List<Suspiciouseness> suspiciousenesses =
@@ -79,24 +106,40 @@ public class KGenProgMain {
         variants.add(variant);
       }
 
+      // この世代で生成された Variants のうち，Fitnessが 1.0 なものを complatedVariants に追加
+      final List<Variant> newComplatedVariants =
+          variants.stream().filter(v -> 0 == Double.compare(v.getFitness().getValue(), 1.0d))
+              .collect(Collectors.toList());
+      completedVariants.addAll(newComplatedVariants);
+
+      // しきい値以上の complatedVariants が生成された場合は，GAを抜ける
+      if (areEnoughComplatedVariants()) {
+        break;
+      }
+
+      // Fitness が 1.0 な Variants は除いた上で，次世代を生成するための Variants を選択
+      variants.removeAll(newComplatedVariants);
       selectedVariants = variantSelection.exec(variants);
     }
   }
 
   // hitori
-  private boolean reachedMaxGeneration() {
-    // TODO Auto-generated method stub
-    return false;
+  private boolean reachedMaxGeneration(final int generation) {
+    return this.maxGeneration <= generation;
   }
 
   // hitori
-  private boolean isTimedOut() {
-    // TODO Auto-generated method stub
-    return false;
+  private boolean isTimedOut(final long startTime) {
+    final long elapsedTime = System.nanoTime() - startTime;
+    return elapsedTime > this.timeout * 1000 * 1000;
   }
 
-  // hitori
+  @Deprecated
   private boolean isSuccess(List<Variant> variants) {
     return false;
+  }
+
+  private boolean areEnoughComplatedVariants() {
+    return this.requiredSolutions <= completedVariants.size();
   }
 }
