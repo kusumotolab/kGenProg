09fe7d360329d25ad1f2790d66674b37cf160cb8
Ryo Arima <r-arima@ist.osaka-u.ac.jp>
2018-07-09 16:08:18 +0900

    Operation::applyでのエラーハンドリング修正

---
+++ src/main/java/jp/kusumotolab/kgenprog/project/GenerationFailedSourceCode.java
@@ -0,0 +1,13 @@
+package jp.kusumotolab.kgenprog.project;
+
+import java.util.Collections;
+
+public class GenerationFailedSourceCode extends GeneratedSourceCode {
+
+  public static GenerationFailedSourceCode GENERATION_FAILED = new GenerationFailedSourceCode();
+
+  public GenerationFailedSourceCode() {
+    super(Collections.emptyList());
+  }
+
+}
