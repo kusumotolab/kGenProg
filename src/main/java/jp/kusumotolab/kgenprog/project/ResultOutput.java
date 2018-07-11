5ca32f63cfa4a43bda93f82892b866d641617673
YoshikiHigo <yoshiki.higo@gmail.com>
2018-07-11 09:22:03 +0900

    merge changes in master

--- src/main/java/jp/kusumotolab/kgenprog/project/ResultOutput.java
+++ src/main/java/jp/kusumotolab/kgenprog/project/ResultOutput.java
@@ -2,6 +2,7 @@ package jp.kusumotolab.kgenprog.project;
 
 import java.util.List;
 import jp.kusumotolab.kgenprog.ga.Variant;
+import jp.kusumotolab.kgenprog.project.factory.TargetProject;
 
 public interface ResultOutput {
   public void outputResult(TargetProject targetProject, List<Variant> modified);
