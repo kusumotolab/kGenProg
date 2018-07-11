e1e32fe3d09a7d47e5bed41616534c5e6f0fbf4e
shinsuke-mat <shinsuke-mat@users.noreply.github.com>
2018-07-11 22:58:20 +0900

    なぜかserialVersionUIDを削除していたので戻した (warnings回避)

--- src/main/java/jp/kusumotolab/kgenprog/project/test/FullyQualifiedName.java
+++ src/main/java/jp/kusumotolab/kgenprog/project/test/FullyQualifiedName.java
@@ -4,6 +4,8 @@ import java.io.Serializable;
 
 public abstract class FullyQualifiedName implements Serializable {
 
+  private static final long serialVersionUID = 1L;
+  
   final public String value;
 
   protected FullyQualifiedName(final String value) {
