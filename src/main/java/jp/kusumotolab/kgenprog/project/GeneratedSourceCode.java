e4c8b5dc19f66c9a16dcff3cdd411e2be69e2634
Ryo Arima <r-arima@ist.osaka-u.ac.jp>
2018-07-17 18:12:11 +0900

    ASTにメッセージダイジェスト (ハッシュ) 生成処理を追加

--- src/main/java/jp/kusumotolab/kgenprog/project/GeneratedSourceCode.java
+++ src/main/java/jp/kusumotolab/kgenprog/project/GeneratedSourceCode.java
@@ -1,9 +1,13 @@
 package jp.kusumotolab.kgenprog.project;
 
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
+import javax.xml.bind.DatatypeConverter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -13,15 +17,19 @@ import org.slf4j.LoggerFactory;
 public class GeneratedSourceCode {
 
   private static Logger log = LoggerFactory.getLogger(GeneratedSourceCode.class);
+  private static final String DIGEST_ALGORITHM = "MD5";
 
   // TODO listは順序が保証されず重複を許容してしまう．Mapで名前から引ける方が外から使いやすい．
-  private List<GeneratedAST> files;
-  private Map<SourceFile, GeneratedAST> fileToAST;
+  private final List<GeneratedAST> files;
+  private final Map<SourceFile, GeneratedAST> fileToAST;
+  private final String messageDigest;
 
   public GeneratedSourceCode(List<GeneratedAST> files) {
     this.files = files;
     fileToAST = files.stream()
         .collect(Collectors.toMap(GeneratedAST::getSourceFile, v -> v));
+    this.messageDigest = createMessageDigest();
+
   }
 
   public List<GeneratedAST> getFiles() {
@@ -53,5 +61,27 @@ public class GeneratedSourceCode {
   public Range inferLineNumbers(Location location) {
     log.debug("enter inferLineNumbers(Location)");
     return location.inferLineNumbers();
+  }
+
+  public String getMessageDigest() {
+    return messageDigest;
+  }
+
+  private String createMessageDigest() {
+    try {
+      final MessageDigest digest = MessageDigest.getInstance(DIGEST_ALGORITHM);
+
+      files.stream()
+          .sorted(Comparator.comparing(v -> v.getSourceFile()
+              .toString()))
+          .map(GeneratedAST::getMessageDigest)
+          .map(String::getBytes)
+          .forEach(digest::update);
+
+      return DatatypeConverter.printHexBinary(digest.digest());
+
+    } catch (NoSuchAlgorithmException e) {
+      throw new RuntimeException(e);
+    }
   }
 }
