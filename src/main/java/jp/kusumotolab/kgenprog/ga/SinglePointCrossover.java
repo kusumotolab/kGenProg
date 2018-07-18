252540e0c1dff9e826165eda82458fe20051c9fb
mtj0928 <mtj0928@gmail.com>
2018-07-18 14:41:28 +0900

    SinglePointCrossoverを実装 #120

--- src/main/java/jp/kusumotolab/kgenprog/ga/SinglePointCrossover.java
+++ src/main/java/jp/kusumotolab/kgenprog/ga/SinglePointCrossover.java
@@ -1,7 +1,9 @@
 package jp.kusumotolab.kgenprog.ga;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -19,7 +21,31 @@ public class SinglePointCrossover implements Crossover {
   @Override
   public List<Gene> exec(final List<Variant> variants) {
     log.debug("enter exec(List<>)");
-    return Collections.emptyList();
+
+
+    if (variants.isEmpty()) {
+      return Collections.emptyList();
+    }
+
+    return variants.stream()
+        .filter(e -> randomNumberGeneration.getBoolean())
+        .filter(e -> !e.getGene().getBases().isEmpty())
+        .map(e -> makeGene(variants))
+        .collect(Collectors.toList());
+  }
+
+  private Gene makeGene(final List<Variant> variants) {
+    final Variant variantA = variants.get(randomNumberGeneration.getInt(variants.size()));
+    final Variant variantB = variants.get(randomNumberGeneration.getInt(variants.size()));
+    final Gene geneA = variantA.getGene();
+    final Gene geneB = variantB.getGene();
+    final List<Base> basesA = geneA.getBases();
+    final List<Base> basesB = geneB.getBases();
+    final int index = randomNumberGeneration.getInt(Math.min(basesA.size(), basesB.size()));
+    final List<Base> subListA = new ArrayList<>(basesA.subList(0, index));
+    final List<Base> subListB = basesB.subList(index, basesB.size());
+    subListA.addAll(subListB);
+    return new SimpleGene(subListA);
   }
 
 }
