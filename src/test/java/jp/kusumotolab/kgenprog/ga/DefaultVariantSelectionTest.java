28dcd1cd3ad24eaef98c91745540189f1bb91956
shinsuke-mat <shinsuke-mat@users.noreply.github.com>
2018-07-19 22:50:58 +0900

    DefaultVariantSelectionTestのexpectを意味的に書き直し

--- src/test/java/jp/kusumotolab/kgenprog/ga/DefaultVariantSelectionTest.java
+++ src/test/java/jp/kusumotolab/kgenprog/ga/DefaultVariantSelectionTest.java
@@ -15,7 +15,8 @@ public class DefaultVariantSelectionTest {
     final List<Variant> variants = new ArrayList<>();
 
     for (int i = 0; i < 10; i++) {
-      final double value = i / ((i % 2 == 0) ? 10.0 : 20.0);
+      final double divider = (i % 2 == 0) ? 10 : 20;
+      final double value = (double) i / divider;
       final SimpleFitness fitness = new SimpleFitness(value);
       variants.add(new Variant(null, fitness, null));
     }
@@ -24,11 +25,13 @@ public class DefaultVariantSelectionTest {
     assertThat(variants).hasSize(10)
         .extracting(Variant::getFitness)
         .extracting(Fitness::getValue)
+        .hasSize(10)
         .containsExactly(0.00d, 0.05d, 0.20d, 0.15d, 0.40d, 0.25d, 0.60d, 0.35d, 0.80d, 0.45d);
 
     assertThat(selectedVariants).hasSize(variantSize)
         .extracting(Variant::getFitness)
         .extracting(Fitness::getValue)
+        .hasSize(5)
         .containsExactly(0.80d, 0.60d, 0.45d, 0.40d, 0.35d);
   }
 
