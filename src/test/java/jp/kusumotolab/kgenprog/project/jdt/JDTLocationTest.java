45931a40476867de6556227fe9763816f7ccc367
shinsuke-mat <shinsuke-mat@users.noreply.github.com>
2018-07-19 16:30:56 +0900

    JDTLocationTestをassertjに + final追加

--- src/test/java/jp/kusumotolab/kgenprog/project/jdt/JDTLocationTest.java
+++ src/test/java/jp/kusumotolab/kgenprog/project/jdt/JDTLocationTest.java
@@ -1,13 +1,14 @@
 package jp.kusumotolab.kgenprog.project.jdt;
 
-import static org.hamcrest.Matchers.is;
-import static org.junit.Assert.assertThat;
+import static org.assertj.core.api.Assertions.assertThat;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 import org.eclipse.jdt.core.dom.CompilationUnit;
 import org.eclipse.jdt.core.dom.MethodDeclaration;
 import org.eclipse.jdt.core.dom.Statement;
 import org.eclipse.jdt.core.dom.TypeDeclaration;
 import org.junit.Test;
+import jp.kusumotolab.kgenprog.ga.Variant;
 import jp.kusumotolab.kgenprog.project.GeneratedSourceCode;
 import jp.kusumotolab.kgenprog.project.Location;
 import jp.kusumotolab.kgenprog.project.Range;
@@ -19,29 +20,33 @@ public class JDTLocationTest {
 
   @Test
   public void testInferLineNumbers() {
-    final TargetProject targetProject = TargetProjectFactory.create("example/example01");
-    final GeneratedSourceCode generatedSourceCode = targetProject.getInitialVariant()
-        .getGeneratedSourceCode();
+    final Path rootDir = Paths.get("example/example01");
+    final TargetProject targetProject = TargetProjectFactory.create(rootDir);
+    final Variant variant = targetProject.getInitialVariant();
+    final GeneratedSourceCode generatedSourceCode = variant.getGeneratedSourceCode();
 
-    GeneratedJDTAST ast = (GeneratedJDTAST) generatedSourceCode.getAst((new TargetSourcePath(
-        Paths.get("example/example01/src/jp/kusumotolab/BuggyCalculator.java"))));
+    final Path path = rootDir.resolve("src/jp/kusumotolab/BuggyCalculator.java");
+    final TargetSourcePath sourcePath = new TargetSourcePath(path);
+    final GeneratedJDTAST ast = (GeneratedJDTAST) generatedSourceCode.getAst(sourcePath);
 
-    CompilationUnit root = ast.getRoot();
-    TypeDeclaration type = (TypeDeclaration) root.types()
+    final CompilationUnit root = ast.getRoot();
+    final TypeDeclaration type = (TypeDeclaration) root.types()
         .get(0);
-    MethodDeclaration method = type.getMethods()[0];
+    final MethodDeclaration method = type.getMethods()[0];
 
-    Statement statement1 = (Statement) method.getBody()
+    final Statement statement1 = (Statement) method.getBody()
         .statements()
         .get(0);
-    Location location1 = new JDTLocation(null, statement1);
-    assertThat(location1.inferLineNumbers(), is(new Range(4, 9)));
+    final Location location1 = new JDTLocation(null, statement1);
 
-    Statement statement2 = (Statement) method.getBody()
+    assertThat(location1.inferLineNumbers()).isEqualTo(new Range(4, 9));
+
+    final Statement statement2 = (Statement) method.getBody()
         .statements()
         .get(1);
-    Location location2 = new JDTLocation(null, statement2);
-    assertThat(location2.inferLineNumbers(), is(new Range(10, 10)));
+    final Location location2 = new JDTLocation(null, statement2);
+
+    assertThat(location2.inferLineNumbers()).isEqualTo(new Range(10, 10));
   }
 
 }
