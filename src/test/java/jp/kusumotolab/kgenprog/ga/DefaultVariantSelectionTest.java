5947ca86b2e39125542b06230582ab00248c3888
mtj0928 <mtj0928@gmail.com>
2018-07-20 17:08:45 +0900

    Variantを選択する際、FitnessがNanのVariantが上位に来るバグを修正

--- src/test/java/jp/kusumotolab/kgenprog/ga/DefaultVariantSelectionTest.java
+++ src/test/java/jp/kusumotolab/kgenprog/ga/DefaultVariantSelectionTest.java
@@ -5,14 +5,16 @@ import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 import org.junit.Test;
 
 public class DefaultVariantSelectionTest {
 
   @Test
-  public void execTest() {
+  public void testExec() {
     final DefaultVariantSelection variantSelection = new DefaultVariantSelection(10);
-    List<Variant> variants = new ArrayList<>();
+    final List<Variant> variants = new ArrayList<>();
 
     List<Variant> resultVariants = variantSelection.exec(variants);
     assertThat(resultVariants.size(), is(0));
@@ -30,5 +32,28 @@ public class DefaultVariantSelectionTest {
     assertThat(resultVariants.get(9)
         .getFitness()
         .getValue(), is(closeTo(0.8, 0.000001)));
+  }
+
+  @Test
+  public void testExecForNan() {
+    final DefaultVariantSelection variantSelection = new DefaultVariantSelection(10);
+    final List<Variant> variants = new ArrayList<>();
+
+    final List<Variant> nanVariants = IntStream.range(0, 10)
+        .mapToObj(e -> new SimpleFitness(Double.NaN))
+        .map(e -> new Variant(null, e, null))
+        .collect(Collectors.toList());
+
+    variants.addAll(nanVariants);
+
+    final List<Variant> result1 = variantSelection.exec(variants);
+    assertThat(result1.size(), is(0));
+
+    final Variant normalVariant = new Variant(null, new SimpleFitness(0.5d), null);
+    variants.add(normalVariant);
+    final List<Variant> result2 = variantSelection.exec(variants);
+    assertThat(result2.size(), is(1));
+    assertThat(result2.get(0)
+        .equals(normalVariant), is(true));
   }
 }
