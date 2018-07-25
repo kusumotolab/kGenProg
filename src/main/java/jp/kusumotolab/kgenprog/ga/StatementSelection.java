7567c5fa003d390c7105e1eb7efd19e298fb4338
mtj0928 <mtj0928@users.noreply.github.com>
2018-07-25 16:59:16 +0900

    指摘内容を修正

--- src/main/java/jp/kusumotolab/kgenprog/ga/StatementSelection.java
+++ src/main/java/jp/kusumotolab/kgenprog/ga/StatementSelection.java
@@ -5,7 +5,7 @@ import org.eclipse.jdt.core.dom.Statement;
 
 public interface StatementSelection {
 
-  public void setCandidates(final List<org.eclipse.jdt.core.dom.Statement> candidates);
+  public void setCandidates(final List<Statement> candidates);
   public Statement exec();
 
 }
