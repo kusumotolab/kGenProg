6f331877b1222587c300026cea42dc00ddf586e7
ty-v1 <y-tomida@ist.osaka-u.ac.jp>
2018-08-22 23:37:30 +0900

    フィールド名の変更とエイリアスの使用

--- src/test/java/jp/kusumotolab/kgenprog/project/factory/EclipseProjectFactoryTest.java
+++ src/test/java/jp/kusumotolab/kgenprog/project/factory/EclipseProjectFactoryTest.java
@@ -1,5 +1,7 @@
 package jp.kusumotolab.kgenprog.project.factory;
 
+import static jp.kusumotolab.kgenprog.testutil.ExampleAlias.Src.Foo;
+import static jp.kusumotolab.kgenprog.testutil.ExampleAlias.Src.FooTest;
 import static org.assertj.core.api.Assertions.assertThat;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -10,8 +12,8 @@ import jp.kusumotolab.kgenprog.project.TestSourcePath;
 
 public class EclipseProjectFactoryTest {
 
-  private final static ClassPath Junit = new ClassPath(
-      Paths.get("example/BuildSuccess06/lib/junit-4.12.jar"));
+  private final static ClassPath JUNIT_CLASSPATH = new ClassPath(
+      Paths.get("example/BuildSuccess06/./lib/junit-4.12.jar"));
 
   @Test
   public void testCreateBySingleEclipseProject() {
@@ -19,13 +21,13 @@ public class EclipseProjectFactoryTest {
     final EclipseProjectFactory eclipseProjectFactory = new EclipseProjectFactory(rootPath);
     final TargetProject project = eclipseProjectFactory.create();
 
-    final ProductSourcePath foo = new ProductSourcePath(rootPath.resolve("src/Foo.java"));
-    final TestSourcePath fooTest = new TestSourcePath(rootPath.resolve("src/FooTest.java"));
+    final ProductSourcePath foo = new ProductSourcePath(rootPath.resolve(Foo));
+    final TestSourcePath fooTest = new TestSourcePath(rootPath.resolve(FooTest));
 
     assertThat(project.rootPath).isSameAs(rootPath);
     assertThat(project.getProductSourcePaths()).containsExactlyInAnyOrder(foo);
     assertThat(project.getTestSourcePaths()).containsExactlyInAnyOrder(fooTest);
-    assertThat(project.getClassPaths()).containsExactlyInAnyOrder(Junit);
+    assertThat(project.getClassPaths()).containsExactlyInAnyOrder(JUNIT_CLASSPATH);
   }
 
 }
