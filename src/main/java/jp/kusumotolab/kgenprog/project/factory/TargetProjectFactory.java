6674d4901e6531a0afbf3e41fd4b6211ca071d28
YoshikiHigo <yoshiki.higo@gmail.com>
2018-08-29 14:18:07 +0900

    rename DefaultProjectFactory to DefaultTargetProjectFactory

--- src/main/java/jp/kusumotolab/kgenprog/project/factory/TargetProjectFactory.java
+++ src/main/java/jp/kusumotolab/kgenprog/project/factory/TargetProjectFactory.java
@@ -48,7 +48,7 @@ public class TargetProjectFactory {
   public static TargetProject create(final Path rootPath, final List<Path> pathsForProductSource,
       final List<Path> pathsForTestSource, List<Path> pathsForClass, JUnitVersion junitVersion) {
     log.info("enter create(Path, List<Path>, List<Path>, List<Path>, JUnitVersion)");
-    return new DefaultProjectFactory(rootPath, pathsForProductSource, pathsForTestSource,
+    return new DefaultTargetProjectFactory(rootPath, pathsForProductSource, pathsForTestSource,
         pathsForClass, junitVersion).create();
   }
 
