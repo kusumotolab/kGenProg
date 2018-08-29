b9bedcf75135c1d323aad4490ed3eb08747382e4
YoshikiHigo <yoshiki.higo@gmail.com>
2018-08-29 14:17:11 +0900

    move the Path-to-ProductSourcePath conversion function to DPF

--- src/main/java/jp/kusumotolab/kgenprog/project/factory/DefaultProjectFactory.java
+++ src/main/java/jp/kusumotolab/kgenprog/project/factory/DefaultProjectFactory.java
@@ -1,8 +1,15 @@
 package jp.kusumotolab.kgenprog.project.factory;
 
+import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import jp.kusumotolab.kgenprog.project.ClassPath;
 import jp.kusumotolab.kgenprog.project.ProductSourcePath;
 import jp.kusumotolab.kgenprog.project.TestSourcePath;
@@ -10,20 +17,26 @@ import jp.kusumotolab.kgenprog.project.factory.JUnitLibraryResolver.JUnitVersion
 
 public class DefaultProjectFactory implements ProjectFactory {
 
+  private static final Logger log = LoggerFactory.getLogger(DefaultProjectFactory.class);
+
   private final Path rootPath;
   private final List<ProductSourcePath> productSourcePaths;
   private final List<TestSourcePath> testSourcePaths;
   private final List<ClassPath> classPaths;
 
-  public DefaultProjectFactory(final Path rootPath,
-      final List<ProductSourcePath> productSourcePaths, final List<TestSourcePath> testSourcePaths,
-      List<ClassPath> classPaths, JUnitVersion junitVersion) {
-    this.rootPath = rootPath;
-    this.productSourcePaths = productSourcePaths;
-    this.testSourcePaths = testSourcePaths;
+  public DefaultProjectFactory(final Path rootPath, final List<Path> pathsForProductSource,
+      final List<Path> pathsForTestSource, List<Path> pathsForClass, JUnitVersion junitVersion) {
 
-    // create new instance to avoid UnsupportedOperationException
-    this.classPaths = new ArrayList<ClassPath>(classPaths);
+    this.rootPath = rootPath;
+    this.productSourcePaths = getFilePaths(pathsForProductSource, ".java").stream()
+        .map(ProductSourcePath::new)
+        .collect(Collectors.toList());
+    this.testSourcePaths = getFilePaths(pathsForTestSource, ".java").stream()
+        .map(TestSourcePath::new)
+        .collect(Collectors.toList());
+    this.classPaths = getFilePaths(pathsForClass, ".class", ".jar").stream()
+        .map(ClassPath::new)
+        .collect(Collectors.toList());
     this.classPaths.addAll(JUnitLibraryResolver.libraries.get(junitVersion));
   }
 
@@ -37,4 +50,40 @@ public class DefaultProjectFactory implements ProjectFactory {
     return true;
   }
 
+  /**
+   * ディレクトリを含みうるList<Path>から，JavaファイルのみのList<Path>を返す
+   * 
+   * @param paths
+   * @return
+   */
+  private static List<Path> getFilePaths(final List<Path> paths, String... suffixes) {
+    final List<Path> javaFilePaths = new ArrayList<>();
+    for (final Path path : paths) {
+
+      if (Files.isRegularFile(path) && endsWith(path, suffixes)) {
+        javaFilePaths.add(path);
+        continue;
+      }
+
+      if (Files.isDirectory(path)) {
+        try {
+          final Set<Path> javaPaths = Files.walk(path)
+              .filter(p -> endsWith(p, suffixes))
+              .collect(Collectors.toSet());
+          javaFilePaths.addAll(javaPaths);
+        } catch (final IOException e) {
+          log.error("failed to read a directory \"{}\"", path.toString());
+        }
+        continue;
+      }
+    }
+
+    return javaFilePaths;
+  }
+
+  private static boolean endsWith(final Path path, final String... suffixes) {
+    final String pathName = path.toString();
+    return Stream.of(suffixes)
+        .anyMatch(s -> pathName.endsWith(s));
+  }
 }
--- src/main/java/jp/kusumotolab/kgenprog/project/factory/TargetProjectFactory.java
+++ src/main/java/jp/kusumotolab/kgenprog/project/factory/TargetProjectFactory.java
@@ -1,18 +1,10 @@
 package jp.kusumotolab.kgenprog.project.factory;
 
-import java.io.IOException;
-import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.Set;
-import java.util.stream.Collectors;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import jp.kusumotolab.kgenprog.project.ClassPath;
-import jp.kusumotolab.kgenprog.project.ProductSourcePath;
-import jp.kusumotolab.kgenprog.project.TestSourcePath;
 import jp.kusumotolab.kgenprog.project.factory.JUnitLibraryResolver.JUnitVersion;
 
 public class TargetProjectFactory {
@@ -54,20 +46,10 @@ public class TargetProjectFactory {
    * @return TargetProject
    */
   public static TargetProject create(final Path rootPath, final List<Path> pathsForProductSource,
-      final List<Path> pathsForTestSource, List<ClassPath> classPaths, JUnitVersion junitVersion) {
-
-    log.info("enter create(Path, List<Path>, List<Path>, List<ClassPath>, JUnitVersion)");
-
-    final List<ProductSourcePath> productSourcePaths =
-        getJavaFilePaths(pathsForProductSource).stream()
-            .map(ProductSourcePath::new)
-            .collect(Collectors.toList());
-    final List<TestSourcePath> testSourcePaths = getJavaFilePaths(pathsForTestSource).stream()
-        .map(TestSourcePath::new)
-        .collect(Collectors.toList());
-
-    return new DefaultProjectFactory(rootPath, productSourcePaths, testSourcePaths, classPaths,
-        junitVersion).create();
+      final List<Path> pathsForTestSource, List<Path> pathsForClass, JUnitVersion junitVersion) {
+    log.info("enter create(Path, List<Path>, List<Path>, List<Path>, JUnitVersion)");
+    return new DefaultProjectFactory(rootPath, pathsForProductSource, pathsForTestSource,
+        pathsForClass, junitVersion).create();
   }
 
   /**
@@ -79,41 +61,5 @@ public class TargetProjectFactory {
   private static List<ProjectFactory> instanceProjectFactories(final Path rootPath) {
     return Arrays.asList(new AntProjectFactory(rootPath), new MavenProjectFactory(rootPath),
         new GradleProjectFactory(rootPath));
-  }
-
-  /**
-   * ディレクトリを含みうるList<Path>から，JavaファイルのみのList<Path>を返す
-   * 
-   * @param paths
-   * @return
-   */
-  private static List<Path> getJavaFilePaths(final List<Path> paths) {
-    final List<Path> javaFilePaths = new ArrayList<>();
-    for (final Path path : paths) {
-
-      if (Files.isRegularFile(path) && endsWith(path, ".java")) {
-        javaFilePaths.add(path);
-        continue;
-      }
-
-      if (Files.isDirectory(path)) {
-        try {
-          final Set<Path> javaPaths = Files.walk(path)
-              .filter(p -> endsWith(p, ".java"))
-              .collect(Collectors.toSet());
-          javaFilePaths.addAll(javaPaths);
-        } catch (final IOException e) {
-          log.error("failed to read a directory \"{}\"", path.toString());
-        }
-        continue;
-      }
-    }
-
-    return javaFilePaths;
-  }
-
-  private static boolean endsWith(final Path path, final String suffix) {
-    return path.toString()
-        .endsWith(suffix);
   }
 }
