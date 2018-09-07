e0080125a6c0ebab47382fba7c49290b80954776
a-tanikd <8575113+a-tanikd@users.noreply.github.com>
2018-09-07 14:43:20 +0900

    Remove redundant this keywords

--- src/main/java/jp/kusumotolab/kgenprog/KGenProgMain.java
+++ src/main/java/jp/kusumotolab/kgenprog/KGenProgMain.java
@@ -64,7 +64,7 @@ public class KGenProgMain {
     mutation.setCandidates(initialVariant.getGeneratedSourceCode()
         .getAsts());
 
-    final StopWatch stopwatch = new StopWatch(this.config.getTimeLimitSeconds());
+    final StopWatch stopwatch = new StopWatch(config.getTimeLimitSeconds());
     stopwatch.start();
     final OrdinalNumber generation = new OrdinalNumber(1);
     final OrdinalNumber foundSolutions = new OrdinalNumber(0);
@@ -137,12 +137,12 @@ public class KGenProgMain {
 
   private boolean reachedMaxGeneration(final OrdinalNumber generation) {
     log.debug("enter reachedMaxGeneration(OrdinalNumber)");
-    return this.config.getMaxGeneration() <= generation.get();
+    return config.getMaxGeneration() <= generation.get();
   }
 
   private boolean areEnoughCompletedVariants(final List<Variant> completedVariants) {
     log.debug("enter areEnoughCompletedVariants(List<Variant>)");
-    return this.config.getRequiredSolutionsCount() <= completedVariants.size();
+    return config.getRequiredSolutionsCount() <= completedVariants.size();
   }
 
   private void logPatch(final List<Variant> completedVariants) {
