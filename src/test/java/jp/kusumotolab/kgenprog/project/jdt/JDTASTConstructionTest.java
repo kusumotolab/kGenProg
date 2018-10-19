4c3989463d8d4e45fd08d06cb1d1d524dd720af2
Ryo Arima <a3636tako@gmail.com>
2018-10-19 15:55:34 +0900

    テストコードに関するレビューコメントを反映

--- src/test/java/jp/kusumotolab/kgenprog/project/jdt/JDTASTConstructionTest.java
+++ src/test/java/jp/kusumotolab/kgenprog/project/jdt/JDTASTConstructionTest.java
@@ -1,5 +1,9 @@
 package jp.kusumotolab.kgenprog.project.jdt;
 
+import static jp.kusumotolab.kgenprog.testutil.ExampleAlias.Src.BAR;
+import static jp.kusumotolab.kgenprog.testutil.ExampleAlias.Src.BAR_TEST;
+import static jp.kusumotolab.kgenprog.testutil.ExampleAlias.Src.FOO;
+import static jp.kusumotolab.kgenprog.testutil.ExampleAlias.Src.FOO_TEST;
 import static org.assertj.core.api.Assertions.assertThat;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -27,15 +31,13 @@ public class JDTASTConstructionTest {
     assertThat(productAsts).hasSize(2)
         .extracting(GeneratedAST::getSourcePath)
         .extracting(p -> p.path)
-        .containsExactlyInAnyOrder(Paths.get("example/BuildSuccess02/src/example/Bar.java"),
-            Paths.get("example/BuildSuccess02/src/example/Foo.java"));
+        .containsExactlyInAnyOrder(basePath.resolve(FOO), basePath.resolve(BAR));
 
     final List<GeneratedAST<TestSourcePath>> testAsts = generatedSourceCode.getTestAsts();
     assertThat(testAsts).hasSize(2)
         .extracting(GeneratedAST::getSourcePath)
         .extracting(p -> p.path)
-        .containsExactlyInAnyOrder(Paths.get("example/BuildSuccess02/src/example/BarTest.java"),
-            Paths.get("example/BuildSuccess02/src/example/FooTest.java"));
+        .containsExactlyInAnyOrder(basePath.resolve(FOO_TEST), basePath.resolve(BAR_TEST));
   }
 
   @Test
@@ -49,4 +51,22 @@ public class JDTASTConstructionTest {
     assertThat(generatedSourceCode.getGenerationMessage()).isNotBlank();
   }
 
+  @Test
+  public void testConstructASTWithNoTestCode() {
+    // テストクラスがない場合の確認
+    final Path basePath = Paths.get("example/BuildSuccess13");
+    final TargetProject targetProject = TargetProjectFactory.create(basePath);
+    final JDTASTConstruction construction = new JDTASTConstruction();
+    final GeneratedSourceCode generatedSourceCode = construction.constructAST(targetProject);
+    final List<GeneratedAST<ProductSourcePath>> productAsts = generatedSourceCode.getProductAsts();
+
+    assertThat(productAsts).hasSize(1)
+        .extracting(GeneratedAST::getSourcePath)
+        .extracting(p -> p.path)
+        .containsExactlyInAnyOrder(basePath.resolve(FOO));
+
+    // テストのASTはゼロのはず
+    final List<GeneratedAST<TestSourcePath>> testAsts = generatedSourceCode.getTestAsts();
+    assertThat(testAsts).hasSize(0);
+  }
 }
