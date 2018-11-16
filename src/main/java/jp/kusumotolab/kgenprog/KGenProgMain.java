951f46db48f84ee9f08aa4e5feed9348de07236b
YoshikiHigo <yoshiki.higo@gmail.com>
2018-11-16 13:12:41 +0900

    log generation summary

--- src/main/java/jp/kusumotolab/kgenprog/KGenProgMain.java
+++ src/main/java/jp/kusumotolab/kgenprog/KGenProgMain.java
@@ -1,6 +1,11 @@
 package jp.kusumotolab.kgenprog;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.Map;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import jp.kusumotolab.kgenprog.fl.FaultLocalization;
@@ -67,11 +72,16 @@ public class KGenProgMain {
 
     while (true) {
 
+      // 新しい世代に入ったことをログ出力
       logGeneration(variantStore.getGenerationNumber());
 
+      stopwatch.split();
+
       // 変異プログラムを生成
-      variantStore.addGeneratedVariants(mutation.exec(variantStore));
-      variantStore.addGeneratedVariants(crossover.exec(variantStore));
+      final List<Variant> variantsByMutation = mutation.exec(variantStore);
+      final List<Variant> variantsByCrossover = crossover.exec(variantStore);
+      variantStore.addGeneratedVariants(variantsByMutation);
+      variantStore.addGeneratedVariants(variantsByCrossover);
 
       // しきい値以上の completedVariants が生成された場合は，GA を抜ける
       if (areEnoughCompletedVariants(variantStore.getFoundSolutions())) {
@@ -93,10 +103,15 @@ public class KGenProgMain {
 
       // 次世代に向けての準備
       variantStore.changeGeneration();
+
+      logGenerationSummary(stopwatch.toString(), variantsByMutation, variantsByCrossover);
     }
 
     // 生成されたバリアントのパッチ出力
     logPatch(variantStore);
+
+    stopwatch.unsplit();
+    log.info("execution time: " + stopwatch.toString());
 
     return variantStore.getFoundSolutions(config.getRequiredSolutionsCount());
   }
@@ -146,5 +161,74 @@ public class KGenProgMain {
         .append(generation.toString())
         .append(" generation.");
     log.info(sb.toString());
+  }
+
+  private void logGenerationSummary(final String timeText, final List<Variant> variantsByMutation,
+      final List<Variant> variantsByCrossover) {
+    final List<Variant> variants = new ArrayList<>();
+    variants.addAll(variantsByMutation);
+    variants.addAll(variantsByCrossover);
+    final StringBuilder sb = new StringBuilder();
+    sb//
+        .append(System.lineSeparator())
+        .append("----------------------------------------------------------------")
+        .append(System.lineSeparator())
+        .append("Elapsed time: ")
+        .append(timeText)
+        .append(System.lineSeparator())
+        .append("Variants: generated ")
+        .append(variantsByMutation.size() + variantsByCrossover.size())
+        .append(", syntax-valid ")
+        .append(count(variants, v -> v.isSyntaxValid()))
+        .append(", build-succeeded ")
+        .append(count(variants, v -> v.isBuildSucceeded()))
+        .append(System.lineSeparator())
+        .append("Fitness: max ")
+        .append(getMaxText(variants))
+        .append(", min ")
+        .append(getMinText(variants))
+        .append(", ave ")
+        .append(getAverage(variants))
+        .append(System.lineSeparator())
+        .append("----------------------------------------------------------------")
+        .append(System.lineSeparator());
+    log.info(sb.toString());
+  }
+
+  private int count(final List<Variant> variants, final Predicate<Variant> p) {
+    return (int) variants.stream()
+        .filter(p)
+        .count();
+  }
+
+  private String getMaxText(final List<Variant> variants) {
+    final Map.Entry<Double, Long> max =
+        Collections.max(getFrequencies(variants).entrySet(), Map.Entry.comparingByKey());
+    return max.getKey() + "(" + max.getValue() + ")";
+  }
+
+  private String getMinText(final List<Variant> variants) {
+    final Map.Entry<Double, Long> min =
+        Collections.min(getFrequencies(variants).entrySet(), Map.Entry.comparingByKey());
+    return min.getKey() + "(" + min.getValue() + ")";
+  }
+
+  private double getAverage(final List<Variant> variants) {
+    return variants.stream()
+        .filter(v -> v.isBuildSucceeded())
+        .mapToDouble(v -> getFitnessValue(v))
+        .average()
+        .getAsDouble();
+  }
+
+  private Map<Double, Long> getFrequencies(final List<Variant> variants) {
+    return variants.stream()
+        .filter(v -> v.isBuildSucceeded())
+        .collect(Collectors.groupingBy(v -> getFitnessValue(v), Collectors.counting()));
+  }
+
+  private double getFitnessValue(final Variant variant) {
+    return variant.getFitness()
+        .getValue();
   }
 }
