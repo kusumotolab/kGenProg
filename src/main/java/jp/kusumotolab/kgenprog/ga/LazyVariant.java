bb7a56eb7063b79d2b0e8f80f13c7e87f8353ba6
mtj0928 <mtj0928@users.noreply.github.com>
2018-12-13 12:10:36 +0900

    add LazyVariant.

---
+++ src/main/java/jp/kusumotolab/kgenprog/ga/LazyVariant.java
@@ -0,0 +1,56 @@
+package jp.kusumotolab.kgenprog.ga;
+
+import java.util.List;
+import io.reactivex.Single;
+import jp.kusumotolab.kgenprog.fl.Suspiciousness;
+import jp.kusumotolab.kgenprog.project.GeneratedSourceCode;
+import jp.kusumotolab.kgenprog.project.test.EmptyTestResults;
+import jp.kusumotolab.kgenprog.project.test.TestResults;
+
+public class LazyVariant extends Variant {
+
+  private final Single<TestResults> testResultsSingle;
+  private final Single<Fitness> fitnessSingle;
+  private final Single<List<Suspiciousness>> suspiciousnessListSingle;
+
+  public LazyVariant(final long id, final int generationNumber,
+      final Gene gene, final GeneratedSourceCode generatedSourceCode,
+      final Single<TestResults> testResultsSingle, final Single<Fitness> fitnessSingle,
+      final Single<List<Suspiciousness>> suspiciousnessListSingle,
+      final HistoricalElement historicalElement) {
+    super(id, generationNumber, gene, generatedSourceCode, null, null, null,
+        historicalElement);
+
+    this.testResultsSingle = testResultsSingle;
+    this.fitnessSingle = fitnessSingle;
+    this.suspiciousnessListSingle = suspiciousnessListSingle;
+
+    this.testResultsSingle.subscribe(v -> this.testResults = v, Throwable::printStackTrace);
+  }
+
+  @Override
+  public boolean isCompleted() {
+    return fitnessSingle.blockingGet()
+        .isMaximum();
+  }
+
+  @Override
+  public boolean isBuildSucceeded() {
+    return EmptyTestResults.class != testResultsSingle.blockingGet().getClass();
+  }
+
+  @Override
+  public TestResults getTestResults() {
+    return this.testResultsSingle.blockingGet();
+  }
+
+  @Override
+  public Fitness getFitness() {
+    return this.fitnessSingle.blockingGet();
+  }
+
+  @Override
+  public List<Suspiciousness> getSuspiciousnesses() {
+    return this.suspiciousnessListSingle.blockingGet();
+  }
+}
