12ee7dbb084c6eafaeeb2d7bbfa75fd20195a466
Keigo01 <k.naitou0130@gmail.com>
2018-12-21 15:21:19 +0900

    rename examples

--- example/BuildSuccess16/test/example/FooTest.java
+++ example/BuildSuccess16/test/example/FooTest.java
@@ -1,30 +1,15 @@
 package example;
 
 import static org.junit.Assert.*;
-import java.io.IOException;
-import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.Arrays;
-import java.util.List;
-import org.junit.Rule;
+import java.nio.file.Paths;
 import org.junit.Test;
-import org.junit.rules.TemporaryFolder;
 
 public class FooTest{
-  @Rule
-  public TemporaryFolder tempFolder = new TemporaryFolder();
-
   @Test
-  public void test01() throws IOException {
-    final String hello = "Hello World";
-    final String fileName = "foo";
-    final Path path = tempFolder.getRoot().toPath().resolve(fileName);
-    final List<String> list = Arrays.asList(hello);
-
+  public void test01() {
     final Foo foo = new Foo();
-    foo.write(path, list);
-
-    final String str = Files.readAllLines(path).get(0);
-    assertEquals(str, hello);
+    final Path path = Paths.get("./example/BuildSuccess15/resources/test.txt");
+    assertEquals(foo.readFile(path), "Hello World");
   }
 }
