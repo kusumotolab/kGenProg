8812599a02dea206ad11c5289dfe637c059c23b0
YoshikiHigo <yoshiki.higo@gmail.com>
2018-12-23 17:56:38 +0900

    add method to calculate similarity of given two genes.

--- src/main/java/jp/kusumotolab/kgenprog/ga/Gene.java
+++ src/main/java/jp/kusumotolab/kgenprog/ga/Gene.java
@@ -1,9 +1,24 @@
 package jp.kusumotolab.kgenprog.ga;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Gene {
+
+  static public double getSimilarity(final Gene geneA, final Gene geneB) {
+
+    final Set<Base> union = new HashSet<>();
+    union.addAll(geneA.getBases());
+    union.addAll(geneB.getBases());
+
+    final Set<Base> intersection = new HashSet<>();
+    intersection.addAll(geneA.getBases());
+    intersection.retainAll(geneB.getBases());
+
+    return (double) intersection.size() / (double) union.size();
+  }
 
   private final List<Base> bases;
 
---
+++ src/test/java/jp/kusumotolab/kgenprog/ga/GeneTest.java
@@ -0,0 +1,39 @@
+package jp.kusumotolab.kgenprog.ga;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import java.util.Arrays;
+import java.util.List;
+import org.junit.Test;
+import jp.kusumotolab.kgenprog.project.NoneOperation;
+import jp.kusumotolab.kgenprog.project.jdt.InsertOperation;
+
+public class GeneTest {
+
+  @Test
+  public void test_getSimilarity() {
+    final Base noneOperationBase1 = new Base(null, new NoneOperation());
+    final Base noneOperationBase2 = new Base(null, new NoneOperation());
+    final Base noneOperationBase3 = new Base(null, new NoneOperation());
+    final Base noneOperationBase4 = new Base(null, new NoneOperation());
+    final Base noneOperationBase5 = new Base(null, new NoneOperation());
+    final Base insertOperationBase1 = new Base(null, new InsertOperation(null));
+    final Base insertOperationBase2 = new Base(null, new InsertOperation(null));
+    final Base insertOperationBase3 = new Base(null, new InsertOperation(null));
+    final Base insertOperationBase4 = new Base(null, new InsertOperation(null));
+    final Base insertOperationBase5 = new Base(null, new InsertOperation(null));
+
+    final List<Base> noneBases = Arrays.asList(noneOperationBase1, noneOperationBase2,
+        noneOperationBase3, noneOperationBase4, noneOperationBase5);
+    final List<Base> insertBases = Arrays.asList(insertOperationBase1, insertOperationBase2,
+        insertOperationBase3, insertOperationBase4, insertOperationBase5);
+
+    final Gene nonBasesGene = new Gene(noneBases);
+    final Gene insertBasesGene = new Gene(insertBases);
+
+    final double similarity1 = Gene.getSimilarity(nonBasesGene, insertBasesGene);
+    assertThat(similarity1).isEqualTo(0.0d);
+
+    final double similarity2 = Gene.getSimilarity(nonBasesGene, nonBasesGene);
+    assertThat(similarity2).isEqualTo(1.0d);
+  }
+}
