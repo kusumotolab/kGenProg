e333217da01aba86db23c8cede1d53b340b97d8e
YoshikiHigo <yoshiki.higo@gmail.com>
2018-12-30 00:41:04 +0900

    bug fix: must use test similarity instead of gene similarity

--- src/main/java/jp/kusumotolab/kgenprog/ga/crossover/TestSimilarityBasedUniformCrossover.java
+++ src/main/java/jp/kusumotolab/kgenprog/ga/crossover/TestSimilarityBasedUniformCrossover.java
@@ -12,7 +12,7 @@ public class TestSimilarityBasedUniformCrossover extends SimilarityBasedUniformC
 
   @Override
   protected double calculateSimilarity(final Variant variant1, final Variant variant2) {
-    return SimilarityCalculator.exec(variant1, variant2, v -> v.getGene()
-        .getBases());
+    return SimilarityCalculator.exec(variant1, variant2, v -> v.getTestResults()
+        .getFailedTestFQNs());
   }
 }
