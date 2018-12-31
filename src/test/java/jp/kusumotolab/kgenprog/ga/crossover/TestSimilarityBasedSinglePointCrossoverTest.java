d43259c17b3036b07187b1ece673df9bda1a246c
YoshikiHigo <yoshiki.higo@gmail.com>
2018-12-31 18:45:25 +0900

    refactor tests for TestSimilarityBasedSinglePointCrossover

--- src/test/java/jp/kusumotolab/kgenprog/ga/crossover/TestSimilarityBasedSinglePointCrossoverTest.java
+++ src/test/java/jp/kusumotolab/kgenprog/ga/crossover/TestSimilarityBasedSinglePointCrossoverTest.java
@@ -3,38 +3,68 @@ package jp.kusumotolab.kgenprog.ga.crossover;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.Mockito.when;
-import java.util.Arrays;
 import java.util.List;
 import java.util.Random;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mockito;
+import jp.kusumotolab.kgenprog.ga.variant.Gene;
+import jp.kusumotolab.kgenprog.ga.variant.HistoricalElement;
 import jp.kusumotolab.kgenprog.ga.variant.Variant;
 
 public class TestSimilarityBasedSinglePointCrossoverTest {
 
-  private static Random random;
+
   private static CrossoverTestVariants testVariants;
 
   @Before
   public void setup() {
-    random = Mockito.mock(Random.class);
-    when(random.nextBoolean()).thenReturn(false);
-    when(random.nextInt(anyInt())).thenReturn(1);
-
     testVariants = new CrossoverTestVariants();
   }
 
   @Test
-  public void test_selectSecondVariant() {
-    final SinglePointCrossover crossover = new TestSimilarityBasedSinglePointCrossover(random, 1);
-    List<Variant> variants = Arrays.asList(testVariants.variantA, testVariants.variantB,
-        testVariants.variantC, testVariants.variantD);
+  public void test_exec1() {
 
-    final Variant variant1 = crossover.selectSecondVariant(variants, testVariants.variantA);
-    assertThat(variant1).isEqualTo(testVariants.variantB);
+    // 生成するバリアントを制御するための疑似乱数
+    final Random random = Mockito.mock(Random.class);
+    when(random.nextBoolean()).thenReturn(true);
+    when(random.nextInt(anyInt())).thenReturn(0);
 
-    final Variant variant2 = crossover.selectSecondVariant(variants, testVariants.variantC);
-    assertThat(variant2).isEqualTo(testVariants.variantD);
+    // バリアントの生成
+    final Crossover crossover = new TestSimilarityBasedSinglePointCrossover(random, 1);
+    final List<Variant> variants = crossover.exec(testVariants.variantStore);
+    final Variant variant = variants.get(0);
+
+    // 1つ目のバリアントとしてvariantA，2つ目のバリアントとしてvariantBが選ばれているはず
+    final HistoricalElement element = variant.getHistoricalElement();
+    assertThat(element.getParents()).containsExactly(testVariants.variantA, testVariants.variantB);
+
+    // 交叉ポイントは0なので，生成されたバリアントのGeneはvariantBと同じになっているはず
+    final Gene gene = variant.getGene();
+    assertThat(gene.getBases()).containsExactly(testVariants.noneBase, testVariants.noneBase,
+        testVariants.noneBase, testVariants.insertBase);
+  }
+
+  @Test
+  public void test_exec2() {
+
+    // 生成するバリアントを制御するための疑似乱数
+    final Random random = Mockito.mock(Random.class);
+    when(random.nextBoolean()).thenReturn(false);
+    when(random.nextInt(anyInt())).thenReturn(2);
+
+    // バリアントの生成
+    final Crossover crossover = new TestSimilarityBasedSinglePointCrossover(random, 1);
+    final List<Variant> variants = crossover.exec(testVariants.variantStore);
+    final Variant variant = variants.get(0);
+
+    // 1つ目のバリアントとしてvariantC，2つ目のバリアントとしてvariantDが選ばれているはず
+    final HistoricalElement element = variant.getHistoricalElement();
+    assertThat(element.getParents()).containsExactly(testVariants.variantC, testVariants.variantD);
+
+    // 交叉ポイントは2なので，0と1はvariantCと同じ，2と3はvarriantDと同じ担っているはず
+    final Gene gene = variant.getGene();
+    assertThat(gene.getBases()).containsExactly(testVariants.noneBase, testVariants.insertBase,
+        testVariants.insertBase, testVariants.insertBase);
   }
 }
