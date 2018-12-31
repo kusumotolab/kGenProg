ed0c40126e5b0932ff9dd69656d7e761bd133cad
YoshikiHigo <yoshiki.higo@gmail.com>
2018-12-31 18:21:58 +0900

    add tests for TestSimilarityBasedUniformCrossover

--- src/test/java/jp/kusumotolab/kgenprog/ga/crossover/CrossoverTestVariants.java
+++ src/test/java/jp/kusumotolab/kgenprog/ga/crossover/CrossoverTestVariants.java
@@ -1,10 +1,17 @@
 package jp.kusumotolab.kgenprog.ga.crossover;
 
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 import java.util.Arrays;
 import org.mockito.Mockito;
+import jp.kusumotolab.kgenprog.ga.variant.Base;
+import jp.kusumotolab.kgenprog.ga.variant.Gene;
+import jp.kusumotolab.kgenprog.ga.variant.HistoricalElement;
 import jp.kusumotolab.kgenprog.ga.variant.Variant;
+import jp.kusumotolab.kgenprog.ga.variant.VariantStore;
+import jp.kusumotolab.kgenprog.project.NoneOperation;
 import jp.kusumotolab.kgenprog.project.TestFullyQualifiedName;
+import jp.kusumotolab.kgenprog.project.jdt.InsertOperation;
 import jp.kusumotolab.kgenprog.project.test.TestResults;
 
 // 10のテストケースが存在すると仮定する．
@@ -14,12 +21,16 @@ import jp.kusumotolab.kgenprog.project.test.TestResults;
 // TestResultsD は5〜10のテストを失敗する．
 public class CrossoverTestVariants {
 
+  final Base noneBase;
+  final Base insertBase;
   final Variant variantA;
   final Variant variantB;
   final Variant variantC;
   final Variant variantD;
+  final VariantStore variantStore;
 
   public CrossoverTestVariants() {
+
     final TestResults testResultsA = Mockito.mock(TestResults.class);
     when(testResultsA.getFailedTestFQNs())
         .thenReturn(Arrays.asList(new TestFullyQualifiedName("Test1"),
@@ -40,13 +51,34 @@ public class CrossoverTestVariants {
         Arrays.asList(new TestFullyQualifiedName("Test5"), new TestFullyQualifiedName("Test6"),
             new TestFullyQualifiedName("Test7"), new TestFullyQualifiedName("Test8"),
             new TestFullyQualifiedName("Test9"), new TestFullyQualifiedName("Test10")));
+
+    noneBase = new Base(null, new NoneOperation());
+    insertBase = new Base(null, new InsertOperation(null));
+    final Gene geneA = new Gene(Arrays.asList(noneBase, noneBase, noneBase, noneBase));
+    final Gene geneB = new Gene(Arrays.asList(noneBase, noneBase, noneBase, insertBase));
+    final Gene geneC = new Gene(Arrays.asList(noneBase, insertBase, insertBase, insertBase));
+    final Gene geneD = new Gene(Arrays.asList(insertBase, insertBase, insertBase, insertBase));
+
     variantA = Mockito.mock(Variant.class);
     when(variantA.getTestResults()).thenReturn(testResultsA);
+    when(variantA.getGene()).thenReturn(geneA);
     variantB = Mockito.mock(Variant.class);
     when(variantB.getTestResults()).thenReturn(testResultsB);
+    when(variantB.getGene()).thenReturn(geneB);
     variantC = Mockito.mock(Variant.class);
     when(variantC.getTestResults()).thenReturn(testResultsC);
+    when(variantC.getGene()).thenReturn(geneC);
     variantD = Mockito.mock(Variant.class);
     when(variantD.getTestResults()).thenReturn(testResultsD);
+    when(variantD.getGene()).thenReturn(geneD);
+
+    variantStore = Mockito.mock(VariantStore.class);
+    when(variantStore.getCurrentVariants())
+        .thenReturn(Arrays.asList(variantA, variantB, variantC, variantD));
+    when(variantStore.createVariant(any(), any())).thenAnswer(invocation -> {
+      final Gene gene = invocation.getArgument(0);
+      final HistoricalElement element = invocation.getArgument(1);
+      return new Variant(0, 0, gene, null, null, null, null, element);
+    });
   }
 }
---
+++ src/test/java/jp/kusumotolab/kgenprog/ga/crossover/TestSimilarityBasedUniformCrossoverTest.java
@@ -0,0 +1,69 @@
+package jp.kusumotolab.kgenprog.ga.crossover;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.Mockito.when;
+import java.util.List;
+import java.util.Random;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mockito;
+import jp.kusumotolab.kgenprog.ga.variant.Gene;
+import jp.kusumotolab.kgenprog.ga.variant.HistoricalElement;
+import jp.kusumotolab.kgenprog.ga.variant.Variant;
+
+public class TestSimilarityBasedUniformCrossoverTest {
+
+  private static CrossoverTestVariants testVariants;
+
+  @Before
+  public void setup() {
+    testVariants = new CrossoverTestVariants();
+  }
+
+  @Test
+  public void test_exec1() {
+
+    // 生成するバリアントを制御するための疑似乱数
+    final Random random = Mockito.mock(Random.class);
+    when(random.nextBoolean()).thenReturn(true);
+    when(random.nextInt(anyInt())).thenReturn(0);
+
+    // バリアントの生成
+    final UniformCrossover crossover = new TestSimilarityBasedUniformCrossover(random, 1);
+    final List<Variant> variants = crossover.exec(testVariants.variantStore);
+    final Variant variant = variants.get(0);
+
+    // 1つ目のバリアントとしてvariantA，2つ目のバリアントとしてvariantBが選ばれているはず
+    final HistoricalElement element = variant.getHistoricalElement();
+    assertThat(element.getParents()).containsExactly(testVariants.variantA, testVariants.variantB);
+
+    // 生成されたバリアントのGeneはvariantAと同じになっているはず
+    final Gene gene = variant.getGene();
+    assertThat(gene.getBases()).containsExactly(testVariants.noneBase, testVariants.noneBase,
+        testVariants.noneBase, testVariants.noneBase);
+  }
+
+  @Test
+  public void test_exec2() {
+
+    // 生成するバリアントを制御するための疑似乱数
+    final Random random = Mockito.mock(Random.class);
+    when(random.nextBoolean()).thenReturn(false);
+    when(random.nextInt(anyInt())).thenReturn(2);
+
+    // バリアントの生成
+    final UniformCrossover crossover = new TestSimilarityBasedUniformCrossover(random, 1);
+    final List<Variant> variants = crossover.exec(testVariants.variantStore);
+    final Variant variant = variants.get(0);
+
+    // 1つ目のバリアントとしてvariantC，2つ目のバリアントとしてvariantDが選ばれているはず
+    final HistoricalElement element = variant.getHistoricalElement();
+    assertThat(element.getParents()).containsExactly(testVariants.variantC, testVariants.variantD);
+
+    // 生成されたバリアントのGeneはvariantDと同じになっているはず
+    final Gene gene = variant.getGene();
+    assertThat(gene.getBases()).containsExactly(testVariants.insertBase, testVariants.insertBase,
+        testVariants.insertBase, testVariants.insertBase);
+  }
+}
