c7c89b7bf1492e733604ee60493b0d4b177ed606
Kaisei Hanayama <hnyma637111@gmail.com>
2019-02-01 22:02:12 +0900

    add EliteAndOldVariantSelection

---
+++ src/main/java/jp/kusumotolab/kgenprog/ga/selection/EliteAndOldVariantSelection.java
@@ -0,0 +1,25 @@
+package jp.kusumotolab.kgenprog.ga.selection;
+
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+import jp.kusumotolab.kgenprog.ga.variant.Variant;
+
+public class EliteAndOldVariantSelection implements VariantSelection {
+
+  final private int maxVariantsPerGeneration;
+
+  public EliteAndOldVariantSelection(int maxVariantPerGeneration) {
+    this.maxVariantsPerGeneration = maxVariantPerGeneration;
+  }
+
+  @Override
+  public List<Variant> exec(final List<Variant> current, final List<Variant> generated) {
+    final List<Variant> list = Stream.concat(current.stream(), generated.stream())
+        .sorted(Comparator.comparing(Variant::getFitness).reversed())
+        .limit(maxVariantsPerGeneration)
+        .collect(Collectors.toList());
+    return list;
+  }
+}
