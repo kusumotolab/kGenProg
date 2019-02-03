38e2b31dfc3f15b0bf694e00933391020bdcc839
YoshikiHigo <yoshiki.higo@gmail.com>
2019-02-03 17:49:00 +0900

    add javadoc

--- src/main/java/jp/kusumotolab/kgenprog/ga/crossover/SecondVariantSelectionStrategy.java
+++ src/main/java/jp/kusumotolab/kgenprog/ga/crossover/SecondVariantSelectionStrategy.java
@@ -2,15 +2,28 @@ package jp.kusumotolab.kgenprog.ga.crossover;
 
 import java.util.List;
 import java.util.Random;
-
 import jp.kusumotolab.kgenprog.ga.variant.Variant;
 
+/**
+ * 交叉の二つ目の親を選択する戦略のインターフェース． 一つ目の親として選択されたバリアントとは異なるバリアントを選択する必要がある．
+ * 
+ * @author higo
+ *
+ */
 public interface SecondVariantSelectionStrategy {
 
+  /**
+   * 交叉の二つ目の親を選択するメソッド．
+   * 
+   * @param variants 親の候補
+   * @param firstVariant 一つ目の親として選択されたバリアント
+   * @return 二つ目の親として選択されたバリアント
+   */
   Variant exec(List<Variant> variants, Variant firstVariant);
 
   public enum Strategy {
     Elite {
+
       @Override
       public SecondVariantSelectionStrategy initialize(final Random random) {
         return new SecondVariantEliteSelection();
@@ -18,6 +31,7 @@ public interface SecondVariantSelectionStrategy {
     },
 
     GeneSimilarity {
+
       @Override
       public SecondVariantSelectionStrategy initialize(final Random random) {
         return new SecondVariantGeneSimilarityBasedSelection();
@@ -25,6 +39,7 @@ public interface SecondVariantSelectionStrategy {
     },
 
     Random {
+
       @Override
       public SecondVariantSelectionStrategy initialize(final java.util.Random random) {
         return new SecondVariantRandomSelection(random);
@@ -32,6 +47,7 @@ public interface SecondVariantSelectionStrategy {
     },
 
     TestSimilarity {
+
       @Override
       public SecondVariantSelectionStrategy initialize(final Random random) {
         return new SecondVariantTestSimilarityBasedSelection();
