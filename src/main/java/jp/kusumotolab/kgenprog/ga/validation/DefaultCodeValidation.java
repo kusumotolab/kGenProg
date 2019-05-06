50bc23268973fa2ff5bd25a9ead1a1f51559296d
mtj0928 <mtj0928@users.noreply.github.com>
2019-05-06 10:42:46 +0900

    add JavaDoc to validation

--- src/main/java/jp/kusumotolab/kgenprog/ga/validation/DefaultCodeValidation.java
+++ src/main/java/jp/kusumotolab/kgenprog/ga/validation/DefaultCodeValidation.java
@@ -3,8 +3,16 @@ package jp.kusumotolab.kgenprog.ga.validation;
 import jp.kusumotolab.kgenprog.project.GeneratedSourceCode;
 import jp.kusumotolab.kgenprog.project.test.TestResults;
 
+/**
+ * テスト通過率をそのまま評価値とする評価方法
+ */
 public class DefaultCodeValidation implements SourceCodeValidation {
 
+  /**
+   * @param sourceCode 評価するソースコード
+   * @param testResults ソースコードのテストの結果
+   * @return 評価値
+   */
   @Override
   public Fitness exec(final GeneratedSourceCode sourceCode, final TestResults testResults) {
     return new SimpleFitness(testResults.getSuccessRate());
