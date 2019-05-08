23a3c7eb1319677b4e671f5c2c1d757f21506dac
ty-v1 <y-tomida@ist.osaka-u.ac.jp>
2019-05-08 12:59:40 +0900

    Optimize import

--- src/test/java/jp/kusumotolab/kgenprog/output/VariantSerializerTest.java
+++ src/test/java/jp/kusumotolab/kgenprog/output/VariantSerializerTest.java
@@ -9,7 +9,6 @@ import org.junit.Before;
 import org.junit.Test;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
-import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import jp.kusumotolab.kgenprog.ga.validation.Fitness;
