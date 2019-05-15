b823ff49831a5f085d2564259c663a9330faf208
shinsuke-mat <shinsuke-mat@users.noreply.github.com>
2019-05-15 10:18:31 +0900

    remove unused import

--- src/test/java/jp/kusumotolab/kgenprog/output/VariantStoreExporterTest.java
+++ src/test/java/jp/kusumotolab/kgenprog/output/VariantStoreExporterTest.java
@@ -1,7 +1,6 @@
 package jp.kusumotolab.kgenprog.output;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import org.junit.Before;
