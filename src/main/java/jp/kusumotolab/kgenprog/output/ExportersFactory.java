43bb65bbaada88b5e97854e7320a7cc2c8f22b00
ty-v1 <y-tomida@ist.osaka-u.ac.jp>
2019-10-22 16:55:24 +0900

    Add ExportersFactory

---
+++ src/main/java/jp/kusumotolab/kgenprog/output/ExportersFactory.java
@@ -0,0 +1,28 @@
+package jp.kusumotolab.kgenprog.output;
+
+import java.util.ArrayList;
+import java.util.List;
+import jp.kusumotolab.kgenprog.Configuration;
+
+/**
+ * Exporterインスタンスを生成するクラス
+ */
+public class ExportersFactory {
+
+  /**
+   * 設定情報に応じたExporterを生成する
+   *
+   * @param config 設定情報
+   */
+  public static List<Exporter> create(final Configuration config) {
+    final PatchGenerator patchGenerator = new PatchGenerator();
+    final List<Exporter> exporters = new ArrayList<>();
+    exporters.add(new PatchExporter(config, patchGenerator));
+
+    if (config.getNeedHistoricalElement()) {
+      exporters.add(new JSONExporter(config, patchGenerator));
+    }
+
+    return exporters;
+  }
+}
