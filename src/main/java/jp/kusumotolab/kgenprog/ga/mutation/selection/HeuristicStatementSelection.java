a4c36ebdc38cb866f18fae354d972f2d9010390e
Yoshiki Higo <yoshiki.higo@gmail.com>
2020-03-31 17:57:14 +0900

    remove unused import statements

--- src/main/java/jp/kusumotolab/kgenprog/ga/mutation/selection/HeuristicStatementSelection.java
+++ src/main/java/jp/kusumotolab/kgenprog/ga/mutation/selection/HeuristicStatementSelection.java
@@ -1,30 +1,9 @@
 package jp.kusumotolab.kgenprog.ga.mutation.selection;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Collections;
 import java.util.List;
 import java.util.Random;
-import java.util.stream.Collectors;
-import org.eclipse.jdt.core.dom.AST;
-import org.eclipse.jdt.core.dom.ASTVisitor;
-import org.eclipse.jdt.core.dom.BreakStatement;
-import org.eclipse.jdt.core.dom.ContinueStatement;
-import org.eclipse.jdt.core.dom.ReturnStatement;
-import org.eclipse.jdt.core.dom.SimpleName;
-import org.eclipse.jdt.core.dom.Statement;
-import org.eclipse.jdt.core.dom.ThrowStatement;
-import com.google.common.collect.HashMultimap;
-import com.google.common.collect.Multimap;
-import jp.kusumotolab.kgenprog.ga.mutation.AccessibleVariableSearcher;
-import jp.kusumotolab.kgenprog.ga.mutation.Query;
-import jp.kusumotolab.kgenprog.ga.mutation.Variable;
-import jp.kusumotolab.kgenprog.ga.mutation.heuristic.ASTAnalyzer;
-import jp.kusumotolab.kgenprog.project.FullyQualifiedName;
 import jp.kusumotolab.kgenprog.project.GeneratedAST;
 import jp.kusumotolab.kgenprog.project.ProductSourcePath;
-import jp.kusumotolab.kgenprog.project.TargetFullyQualifiedName;
-import jp.kusumotolab.kgenprog.project.jdt.GeneratedJDTAST;
 import jp.kusumotolab.kgenprog.project.jdt.ProgramElementVisitor;
 import jp.kusumotolab.kgenprog.project.jdt.StatementListVisitor;
 
