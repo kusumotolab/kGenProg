efdc949c12ce9e946c1678fb4fbb2e44d1fc16bf
hnymA <hnyma637111@gmail.com>
2020-06-18 14:41:14 +0900

    remove unused import

--- src/main/java/jp/kusumotolab/kgenprog/ga/mutation/selection/HeuristicSelection.java
+++ src/main/java/jp/kusumotolab/kgenprog/ga/mutation/selection/HeuristicSelection.java
@@ -1,7 +1,6 @@
 package jp.kusumotolab.kgenprog.ga.mutation.selection;
 
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 import java.util.Random;
