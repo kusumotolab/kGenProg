2f9232816a5e4cbd521989a1ef92bbc73b8111a0
ty-v1 <y-tomida@ist.osaka-u.ac.jp>
2020-08-21 19:40:02 +0900

    add test cases

---
+++ example/BuildSuccess25/src/example/Foo.java
@@ -0,0 +1,19 @@
+package example;
+
+public class Foo {
+
+  /**
+   * 2‚Â‚Ì®”‚Ì‚¤‚¿‘å‚«‚¢®”‚ğ•Ô‚·
+   *
+   * @param n ®”
+   * @param m ®”
+   * @return n, m‚Ì‚¤‚¿‘å‚«‚¢®”
+   */
+  public int max(int n, int m) {
+    if (n < m) {
+      return m;
+    } else {
+      return n;
+    }
+  }
+}
