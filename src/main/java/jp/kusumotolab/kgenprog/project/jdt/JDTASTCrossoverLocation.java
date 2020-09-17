4c0fd06002c7d0ab97d0e69228aaf233ea6d2c46
hrtwt <29752757+hrtwt@users.noreply.github.com>
2020-09-17 14:10:41 +0900

    implemented JDTASTCrossoverLocation

--- src/main/java/jp/kusumotolab/kgenprog/project/jdt/JDTASTCrossoverLocation.java
+++ src/main/java/jp/kusumotolab/kgenprog/project/jdt/JDTASTCrossoverLocation.java
@@ -1,6 +1,13 @@
 package jp.kusumotolab.kgenprog.project.jdt;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.eclipse.jdt.core.dom.ASTNode;
+import org.eclipse.jdt.core.dom.StructuralPropertyDescriptor;
 import jp.kusumotolab.kgenprog.project.SourcePath;
 
 public class JDTASTCrossoverLocation extends JDTASTLocation {
@@ -16,8 +23,55 @@ public class JDTASTCrossoverLocation extends JDTASTLocation {
 
   @Override
   public ASTNode locate(final ASTNode otherASTRoot) {
-    // call super as mock
-    return super.locate(otherASTRoot);
+    final List<ASTNode> treePaths = new ArrayList<>();
+    ASTNode currentNode = node;
+    do {
+      treePaths.add(currentNode);
+      currentNode = currentNode.getParent();
+    } while (currentNode != null);
+
+    treePaths.remove(treePaths.size() - 1);
+    Collections.reverse(treePaths);
+
+    List<ASTNode> possibleNode = new ArrayList<>();
+    possibleNode.add(otherASTRoot);
+    for (final ASTNode path : treePaths) {//これと比べる．これが正解．
+      final List<ASTNode> nextPossibleNode = new ArrayList<>();
+      for (final ASTNode current : possibleNode) {
+        getChildren(current).stream()
+            .filter(e -> isSameASTNode(path, e))
+            .forEach(nextPossibleNode::add);
+      }
+      possibleNode = nextPossibleNode;
+    }
+
+    possibleNode = possibleNode.stream()
+        .filter(e -> treePaths.get(treePaths.size() - 1)
+            .toString()
+            .equals(e.toString()))
+        .collect(Collectors.toList());
+
+    return possibleNode.isEmpty() ? null : possibleNode.get(0);
   }
 
+  private List<ASTNode> getChildren(ASTNode node) {
+    List<ASTNode> children = new ArrayList<>();
+    for (final Object o : node.structuralPropertiesForType()) {
+      Object child = node.getStructuralProperty((StructuralPropertyDescriptor) o);
+      if (child instanceof ASTNode) {
+        children.add((ASTNode) child);
+      } else if (child instanceof List) {
+        @SuppressWarnings("unchecked") // List の要素は必ず ASTNode
+        final List<ASTNode> c = (List<ASTNode>) child;
+        children.addAll(c);
+      }
+    }
+    return children;
+  }
+
+  private boolean isSameASTNode(ASTNode a, ASTNode b) {
+//    return a.toString()
+//        .equals(b.toString());// TODO: 正しくない．
+    return a.getClass() == b.getClass();
+  }
 }
